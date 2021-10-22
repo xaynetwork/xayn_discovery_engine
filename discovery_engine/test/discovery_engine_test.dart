3cc73a25c9bd5ee80679b682be8a495b10de3ae1
Pawel Biernacki <pawel@robotygraficzne.com>
2021-10-22 09:42:03 +0200

    TY-2150 Boilerplate for Discovery Engine package

---
+++ discovery_engine/test/discovery_engine_test.dart
@@ -0,0 +1,16 @@
+// import 'package:discovery_engine/discovery_engine.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    // final engine = DiscoveryEngine();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      // TODO: write tests
+    });
+  });
+}
