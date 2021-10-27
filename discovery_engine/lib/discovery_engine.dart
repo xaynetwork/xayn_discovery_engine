833f1964a7f4f0e8d83451baa5627af1c4ddf057
Pawel Biernacki <pawel@robotygraficzne.com>
2021-10-27 11:07:09 +0200

    adjusted library exports

--- discovery_engine/lib/discovery_engine.dart
+++ discovery_engine/lib/discovery_engine.dart
@@ -3,6 +3,5 @@
 /// More dartdocs go here.
 library discovery_engine;
 
-export 'src/discovery_engine_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'package:xayn_discovery_engine/src/api/api.dart';
+export 'package:xayn_discovery_engine/src/discovery_engine_base.dart';
