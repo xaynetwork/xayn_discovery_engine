43fdded42c2689f0469b345e5de74ff5443a5108
Pawel Biernacki <pawel@robotygraficzne.com>
2021-11-03 10:35:27 +0100

    added implementation of platform workers for web and native

---
+++ discovery_engine/lib/src/worker/native/platform_worker_io.dart
@@ -0,0 +1,32 @@
+import 'dart:async' show Stream;
+import 'dart:isolate' show ReceivePort, SendPort;
+
+import 'package:xayn_discovery_engine/src/worker/common/platform_actors.dart'
+    show PlatformWorker;
+
+class _IsolatedWorker extends PlatformWorker {
+  final ReceivePort _workerChannel;
+  final SendPort _managerChannel;
+
+  _IsolatedWorker(SendPort sendPort)
+      : _managerChannel = sendPort,
+        _workerChannel = ReceivePort() {
+    // send the isolate port as the first message
+    _managerChannel.send(_workerChannel.sendPort);
+  }
+
+  @override
+  Stream get messages => _workerChannel.asBroadcastStream();
+
+  @override
+  void send(dynamic message, [List<Object>? transfer]) =>
+      _managerChannel.send(message);
+
+  @override
+  void dispose() {
+    _workerChannel.close();
+  }
+}
+
+PlatformWorker createPlatformWorker(dynamic message) =>
+    _IsolatedWorker(message as SendPort);
---
+++ discovery_engine/lib/src/worker/web/platform_worker_web.dart
@@ -0,0 +1,22 @@
+import 'dart:async' show Stream;
+import 'dart:html' show DedicatedWorkerGlobalScope;
+
+import 'package:xayn_discovery_engine/src/worker/common/platform_actors.dart'
+    show PlatformWorker;
+
+class _WebWorker extends PlatformWorker {
+  DedicatedWorkerGlobalScope get _context =>
+      DedicatedWorkerGlobalScope.instance;
+
+  @override
+  Stream get messages => _context.onMessage.map<dynamic>((event) => event.data);
+
+  @override
+  void send(dynamic message, [List<Object>? transfer]) =>
+      _context.postMessage(message, transfer);
+
+  @override
+  void dispose() => _context.close();
+}
+
+PlatformWorker createPlatformWorker(dynamic message) => _WebWorker();
