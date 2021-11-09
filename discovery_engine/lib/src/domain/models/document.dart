786a235075004ed36566db4a02c2042ac92aad87
kwok <kwok.cheung@xain.io>
2021-11-09 15:33:02 +0000

    TY-2160 make documents hive-storable (#7)
    * generate hive typeadapter for document
    
    * review comments I
    
    * remove hive_flutter dep
    
    * pin hive dep version
    
    * type id constants
    
    * review comments II
    
    * pin dev deps
    
    * move consts to separate file
    
    * show hive imports
    
    * bump test dep version to resolve conflict with hive_generator
    
    * review comment III
    
    * automate code gen in ci
    
    * use dart command to generate code
    
    * bump lints version
    
    * add DART_WORKSPACE to env
    
    * bump build_runner version

--- discovery_engine/lib/src/domain/models/document.dart
+++ discovery_engine/lib/src/domain/models/document.dart
@@ -1,16 +1,29 @@
+import 'package:hive/hive.dart'
+    show HiveType, HiveField, TypeAdapter, BinaryReader, BinaryWriter;
 import 'package:xayn_discovery_engine/src/domain/models/unique_id.dart'
     show DocumentId;
 import 'package:xayn_discovery_engine/src/domain/models/web_resource.dart'
     show WebResource;
+import 'package:xayn_discovery_engine/src/domain/repository/type_id.dart'
+    show documentTypeId, documentFeedbackTypeId, documentStatusTypeId;
+
+part 'document.g.dart';
 
 /// [Document] is representing items in the discovery feed
 /// or in the search result list.
+@HiveType(typeId: documentTypeId)
 class Document {
+  @HiveField(0)
   final DocumentId documentId;
+  @HiveField(1)
   final WebResource webResource;
+  @HiveField(2)
   final DocumentFeedback _feedback;
+  @HiveField(3)
   final DocumentStatus _status;
+  @HiveField(4)
   final int nonPersonalizedRank;
+  @HiveField(5)
   final int personalizedRank;
 
   bool get isRelevant => _feedback == DocumentFeedback.positive;
@@ -21,7 +34,7 @@ class Document {
   int currentRank(bool isPersonalisationOn) =>
       isPersonalisationOn ? personalizedRank : nonPersonalizedRank;
 
-  Document._({
+  Document({
     required this.webResource,
     required this.nonPersonalizedRank,
     required this.personalizedRank,
@@ -30,8 +43,8 @@ class Document {
         _status = DocumentStatus.missed;
 }
 
-/// [DocumentStatus] indicates what is the document status in context of other
-/// documents.
+/// [DocumentStatus] indicates what the document status is in the context of
+/// other documents.
 ///   - Every document starts with `missed`, which means the user didn't have
 /// the chance to see it.
 ///   - When a document is displayed to the user its status is updated to
@@ -39,19 +52,28 @@ class Document {
 ///   - When the user decides to read it, the status is updated to `opened`.
 ///   - When the user decides that the document is not relevant, and scrolls
 /// further, the status is updated to `skipped`.
-///   - Sometimes if user changes the decision a `skipped` document can become
-/// `opened`.
+///   - Sometimes if the user changes the decision a `skipped` document can
+/// become `opened`.
+@HiveType(typeId: documentStatusTypeId)
 enum DocumentStatus {
+  @HiveField(0)
   skipped,
+  @HiveField(1)
   presented,
+  @HiveField(2)
   opened,
+  @HiveField(3)
   missed,
 }
 
 /// [DocumentFeedback] indicates user's "sentiment" towards the document,
 /// essentially if the user "liked" or "disliked" the document.
+@HiveType(typeId: documentFeedbackTypeId)
 enum DocumentFeedback {
+  @HiveField(0)
   neutral,
+  @HiveField(1)
   positive,
+  @HiveField(2)
   negative,
 }
