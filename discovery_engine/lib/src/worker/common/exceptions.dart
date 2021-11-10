741b82647007106e6ce44466668341da7c7218f9
Pawel Biernacki <pawel@robotygraficzne.com>
2021-11-10 20:26:12 +0100

    added initial exceptions

---
+++ discovery_engine/lib/src/worker/common/exceptions.dart
@@ -0,0 +1,36 @@
+/// Thrown when a [Worker] cannot be created.
+class WorkerSpawnException implements Exception {
+  /// Message (or string representation of the exception).
+  final String message;
+
+  /// String representation of the stack trace associated to the original exception.
+  final String? stackTrace;
+
+  WorkerSpawnException(this.message, {String? stackTrace})
+      : stackTrace = stackTrace ?? StackTrace.current.toString();
+
+  @override
+  String toString() => 'WorkerSpawnException: $message\n$stackTrace';
+}
+
+/// Thrown when a [Converter] cannot convert a message.
+class ConverterException implements Exception {
+  /// Message (or string representation of the exception).
+  final String message;
+
+  ConverterException(this.message);
+
+  @override
+  String toString() => 'ConverterException: $message';
+}
+
+/// Thrown when the [Response] takes too long to finish.
+class ResponseTimeoutException implements Exception {
+  /// Message (or string representation of the exception).
+  final String message;
+
+  ResponseTimeoutException(this.message);
+
+  @override
+  String toString() => 'ResponseTimeoutException: $message';
+}
--- discovery_engine/lib/src/worker/worker.dart
+++ discovery_engine/lib/src/worker/worker.dart
@@ -1,3 +1,5 @@
+export 'package:xayn_discovery_engine/src/worker/common/exceptions.dart'
+    show WorkerSpawnException, ResponseTimeoutException, ConverterException;
 export 'package:xayn_discovery_engine/src/worker/common/manager.dart'
     show Manager;
 export 'package:xayn_discovery_engine/src/worker/common/oneshot.dart'
