7a2d96ccf427cda95765e8f7a0de6ea3c795feb9
Kwok Cheung <kwok.doc@gmail.com>
2021-11-12 16:35:08 +0100

    update type signature

--- discovery_engine/lib/src/domain/repository/document_repository.dart
+++ discovery_engine/lib/src/domain/repository/document_repository.dart
@@ -7,5 +7,5 @@ import 'package:xayn_discovery_engine/src/domain/models/unique_id.dart'
 abstract class DocumentRepository {
   Future<Document?> fetchById(DocumentId id);
   Future<List<Document>> fetchAll();
-  Future<void> update(); // FIXME type signature
+  Future<void> update(Document doc);
 }
