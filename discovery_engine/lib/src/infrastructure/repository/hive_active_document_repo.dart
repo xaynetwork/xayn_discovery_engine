a19106e69360bd9273c43e0f4325aeffa26447c9
Kwok Cheung <kwok.doc@gmail.com>
2021-11-18 11:26:23 +0100

    comment out unused import

--- discovery_engine/lib/src/infrastructure/repository/hive_active_document_repo.dart
+++ discovery_engine/lib/src/infrastructure/repository/hive_active_document_repo.dart
@@ -1,5 +1,5 @@
 import 'dart:typed_data' show Uint8List;
-import 'package:hive/hive.dart' show Hive, Box;
+// import 'package:hive/hive.dart' show Hive, Box;
 import 'package:xayn_discovery_engine/src/domain/models/unique_id.dart'
     show DocumentId;
 import 'package:xayn_discovery_engine/src/domain/repository/active_document_repository.dart'
