065e5a15048f71da88e49baa6998f2bf9441e6ed
Pawel Biernacki <pawel@robotygraficzne.com>
2021-11-18 10:10:12 +0100

    review comments

--- discovery_engine_core/core/src/lib.rs
+++ discovery_engine_core/core/src/lib.rs
@@ -1,3 +1,16 @@
+//! Xayn Discovery Engine core.
+#![warn(missing_docs, unreachable_pub)]
+#![forbid(
+    noop_method_call,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unsafe_code,
+    unsafe_op_in_unsafe_fn,
+    unused_qualifications
+)]
+#![deny(clippy::pedantic)]
+#![deny(clippy::future_not_send)]
+
 #[cfg(test)]
 mod tests {
     #[test]
