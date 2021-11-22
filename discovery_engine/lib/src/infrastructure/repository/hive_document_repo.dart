b02111e076d5e10cb55adb6fa96919d702381e98
Kwok Cheung <kwok.doc@gmail.com>
2021-11-22 11:04:34 +0100

    simplify doc repo impl

--- discovery_engine/lib/src/infrastructure/repository/hive_document_repo.dart
+++ discovery_engine/lib/src/infrastructure/repository/hive_document_repo.dart
@@ -8,26 +8,13 @@ import 'package:xayn_discovery_engine/src/domain/repository/document_repository.
 import 'package:xayn_discovery_engine/src/infrastructure/box_name.dart'
     show documentBox;
 
+/// Hive repository for accessing documents.
 class HiveDocumentRepository implements DocumentRepository {
-  bool _isLoaded = false;
-  final _idDocMap = <DocumentId, Document>{};
-  final _idKeyMap = <DocumentId, dynamic>{};
-
   Box<Document> get box => Hive.box<Document>(documentBox);
-
-  void _loadMaps() {
-    box.toMap().forEach((dynamic key, doc) {
-      _idDocMap[doc.documentId] = doc;
-      _idKeyMap[doc.documentId] = key;
-    });
-
-    _isLoaded = true;
-  }
 
   @override
   Future<Document?> fetchById(DocumentId id) async {
-    if (!_isLoaded) _loadMaps();
-    return _idDocMap[id];
+    return box.get(id.toString());
   }
 
   @override
@@ -35,18 +22,7 @@ class HiveDocumentRepository implements DocumentRepository {
 
   @override
   Future<void> update(Document doc) async {
-    if (!_isLoaded) _loadMaps();
-
-    dynamic key = _idKeyMap[doc.documentId];
-    if (key == null) {
-      // add new doc to box, generating a new db key
-      key = await box.add(doc);
-      _idKeyMap[doc.documentId] = key;
-    } else {
-      // update existing doc in box
-      await box.put(key, doc);
-    }
-
-    _idDocMap[doc.documentId] = doc;
+    var key = doc.documentId.toString();
+    await box.put(key, doc);
   }
 }
