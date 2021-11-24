e86855eca6b8f94d46170b7c7ef2eadb8e5b6e1b
Pawel Biernacki <pawel@robotygraficzne.com>
2021-11-24 14:48:12 +0100

    better arrangement of linters

--- discovery_engine_core/core/src/lib.rs
+++ discovery_engine_core/core/src/lib.rs
@@ -1,15 +1,14 @@
 //! Xayn Discovery Engine core.
-#![warn(missing_docs, unreachable_pub)]
+#![forbid(unsafe_code, unsafe_op_in_unsafe_fn)]
 #![deny(
+    clippy::pedantic,
+    clippy::future_not_send,
     noop_method_call,
     rust_2018_idioms,
     rust_2021_compatibility,
-    unsafe_code,
-    unsafe_op_in_unsafe_fn,
     unused_qualifications
 )]
-#![deny(clippy::pedantic)]
-#![deny(clippy::future_not_send)]
+#![warn(missing_docs, unreachable_pub)]
 #![allow(clippy::missing_errors_doc, clippy::must_use_candidate)]
 
 mod document;
