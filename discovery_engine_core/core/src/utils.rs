a739ce1150134759b0d17fad00091bbef051a6e8
Robert Debug <robertt.debug@gmail.com>
2021-11-26 18:22:55 +0100

    fix comment

--- discovery_engine_core/core/src/utils.rs
+++ discovery_engine_core/core/src/utils.rs
@@ -7,7 +7,6 @@ use std::cmp::Ordering;
 /// `NaN` is treated as the lowest possible value if `nan_min`, similar to what [`f32::max`] does.
 /// Otherwise it is treated as the highest possible value, similar to what [`f32::min`] does.
 #[allow(clippy::trivially_copy_pass_by_ref)]
-// we allow the lint because we may want to use the function for `std::slice::sort_by`
 pub(crate) fn nan_safe_f32_cmp_base(a: &f32, b: &f32, nan_min: bool) -> Ordering {
     a.partial_cmp(b).unwrap_or_else(|| {
         // if `partial_cmp` returns None we have at least one `NaN`,
@@ -37,6 +36,7 @@ pub(crate) fn nan_safe_f32_cmp_base(a: &f32, b: &f32, nan_min: bool) -> Ordering
 /// By switching the input parameters around this can be used to create a
 /// descending sorted order, like e.g.: `[2.0, 1.5, 0.5, NaN]`.
 #[allow(clippy::trivially_copy_pass_by_ref)]
+// we allow the lint because we may want to use the function for `std::slice::sort_by`
 pub(crate) fn nan_safe_f32_cmp(a: &f32, b: &f32) -> Ordering {
     nan_safe_f32_cmp_base(a, b, true)
 }
