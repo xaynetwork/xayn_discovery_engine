c0eb5d6824a2604b7bed4cbf8f132a456145a91e
Kwok Cheung <kwok.doc@gmail.com>
2021-12-02 02:31:05 +0100

    handle feed docs closed by setting them inactive

--- discovery_engine/lib/src/domain/document_manager.dart
+++ discovery_engine/lib/src/domain/document_manager.dart
@@ -1,34 +1,65 @@
 import 'package:xayn_discovery_engine/src/api/events/client_events/document_events.dart'
     show DocumentClientEvent;
+import 'package:xayn_discovery_engine/src/api/events/client_events/feed_events.dart'
+    show FeedClientEvent;
 import 'package:xayn_discovery_engine/src/domain/models/document.dart'
-    show DocumentFeedback, DocumentStatus;
+    show DocumentFeedback;
 import 'package:xayn_discovery_engine/src/domain/models/unique_id.dart'
     show DocumentId;
+import 'package:xayn_discovery_engine/src/domain/repository/active_document_repo.dart'
+    show ActiveDocumentDataRepository;
+import 'package:xayn_discovery_engine/src/domain/repository/changed_document_repo.dart'
+    show ChangedDocumentRepository;
 import 'package:xayn_discovery_engine/src/domain/repository/document_repo.dart'
     show DocumentRepository;
 
 class DocumentManager {
-  final DocumentRepository _repo;
+  final DocumentRepository _documentRepo;
+  final ActiveDocumentDataRepository _activeRepo;
+  final ChangedDocumentRepository _changedRepo;
 
-  DocumentManager(this._repo);
+  DocumentManager(this._documentRepo, this._activeRepo, this._changedRepo);
 
-  void handle(DocumentClientEvent evt) {
+  void handleDocumentEvent(DocumentClientEvent evt) {
     evt.when(
-      documentStatusChanged: (DocumentId id, DocumentStatus status) =>
-          print('baz'),
-      documentClosed: (DocumentId id) => print('bar'),
-      documentFeedbackChanged: (DocumentId id, DocumentFeedback feedback) =>
-          handleFeedbackChanged(id, feedback),
+      documentStatusChanged: (id, status) => print('baz'),
+      documentClosed: (id) => print('bar'),
+      documentFeedbackChanged: (id, feedback) =>
+          updateDocumentFeedback(id, feedback),
     );
   }
 
-  /// Handle [DocumentClientEvent.documentFeedbackChanged].
-  Future<void> handleFeedbackChanged(
+  /// Update the feedback for the given document.
+  ///
+  /// Has no effect if `id` does not identify an active document.
+  Future<void> updateDocumentFeedback(
     DocumentId id,
     DocumentFeedback feedback,
   ) async {
-    final doc = await _repo.fetchById(id);
-    final updatedDoc = doc?.setFeedback(feedback);
-    await _repo.update(updatedDoc!);
+    final doc = await _documentRepo.fetchById(id);
+    if (doc != null && doc.isActive) {
+      final updatedDoc = doc.setFeedback(feedback);
+      await _documentRepo.update(updatedDoc);
+    }
+  }
+
+  void handleFeedEvent(FeedClientEvent evt) {
+    evt.when(
+      feedDocumentsClosed: (ids) => deactivateDocuments(ids),
+      nextFeedBatchRequested: () => print('bar'),
+      feedRequested: () => print('baz'),
+    );
+  }
+
+  /// Deactivate the given documents.
+  Future<void> deactivateDocuments(Set<DocumentId> ids) async {
+    await _activeRepo.removeByIds(ids);
+    await _changedRepo.removeMany(ids);
+
+    for (final id in ids) {
+      var doc = await _documentRepo.fetchById(id);
+      doc = doc?.setInactive();
+      if (doc != null) await _documentRepo.update(doc);
+    }
   }
 }
