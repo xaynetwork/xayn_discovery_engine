171dc61c704eaa9afbbdc5bffc4aaccf074edfce
Andrea Corradi <andrea.corradi@xayn.com>
2021-12-16 12:19:18 +0100

    TY 2234 generalize stack (#28)

--- discovery_engine_core/core/src/engine.rs
+++ discovery_engine_core/core/src/engine.rs
@@ -1,46 +1,70 @@
+use std::collections::HashMap;
+
 use displaydoc::Display;
-use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
-use crate::{document::Document, stack::Stack};
+use crate::{
+    document::Document,
+    stack::{BoxedOps, Data as StackData, Id as StackId, Stack},
+};
 
 #[derive(Error, Debug, Display)]
 pub enum Error {
     /// Failed to serialize internal state of the engine: {0}.
     Serialization(#[source] bincode::Error),
+
     /// Failed to deserialize internal state to create the engine: {0}.
     Deserialization(#[source] bincode::Error),
+
+    /// No operations on stack were provided.
+    NoStackOps,
 }
 
 /// Discovery Engine.
 pub struct Engine {
-    /// Internal state of the engine.
-    state: InternalState,
+    stacks: HashMap<StackId, Stack>,
 }
 
 impl Engine {
-    /// Creates a new [`Engine`] from serialized state.
-    pub fn new(state: &[u8]) -> Result<Self, Error> {
-        let state = bincode::deserialize(state).map_err(Error::Deserialization)?;
-        Ok(Engine { state })
+    /// Creates a new `Engine` from serialized state and stack operations.
+    ///
+    /// The `Engine` only keeps in its state data related to the current [`BoxStackOps`].
+    /// Data related to missing operations will be dropped.
+    pub fn new(state: &[u8], stacks_ops: Vec<BoxedOps>) -> Result<Self, Error> {
+        if stacks_ops.is_empty() {
+            return Err(Error::NoStackOps);
+        }
+
+        let mut stacks_data: HashMap<StackId, StackData> =
+            bincode::deserialize(state).map_err(Error::Deserialization)?;
+
+        let stacks = stacks_ops
+            .into_iter()
+            .map(|ops| {
+                let id = ops.id();
+                let data = stacks_data.remove(&id).unwrap_or_default();
+                (id, Stack::new(data, ops))
+            })
+            .collect();
+
+        Ok(Engine { stacks })
     }
 
-    /// Serializes [`InternalState`] of the engine.
+    /// Serializes the state of the `Engine`.
+    ///
+    /// The result can be used with [`Engine::new`] to restore it.
     pub fn serialize(&self) -> Result<Vec<u8>, Error> {
-        bincode::serialize(&self.state).map_err(Error::Serialization)
+        let stacks_data: HashMap<&StackId, &StackData> = self
+            .stacks
+            .iter()
+            .map(|(id, stack)| (id, &stack.data))
+            .collect();
+
+        bincode::serialize(&stacks_data).map_err(Error::Serialization)
     }
 }
 
-/// Internal state of [`Engine`].
-#[derive(Deserialize, Serialize)]
-pub(crate) struct InternalState {
-    /// Stack of news in a news feed.
-    pub(crate) news_feed: Stack,
-    /// Stack of personalized news.
-    pub(crate) personalized_news: Stack,
-}
-
-/// A a wrapper around a dynamic error type, similar to `anyhow::Error`,
+/// A wrapper around a dynamic error type, similar to `anyhow::Error`,
 /// but without the need to declare `anyhow` as a dependency.
 pub(crate) type GenericError = Box<dyn std::error::Error + Sync + Send + 'static>;
 
---
+++ discovery_engine_core/core/src/stack/data.rs
@@ -0,0 +1,96 @@
+use derivative::Derivative;
+use displaydoc::Display;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use crate::document::Document;
+
+#[derive(Error, Debug, Display)]
+#[allow(dead_code)]
+pub(crate) enum Error {
+    /// Invalid value for alpha: {0}. It must be greater than 0.
+    InvalidAlpha(f32),
+    /// Invalid value for beta: {0}. It must be greater than 0.
+    InvalidBeta(f32),
+}
+
+/// Common data of a [`Stack`](super::Stack).
+#[derive(Derivative, Deserialize, Serialize, Debug)]
+#[derivative(Default)]
+pub(crate) struct Data {
+    /// The alpha parameter of the beta distribution.
+    #[derivative(Default(value = "1."))]
+    pub(super) alpha: f32,
+    /// The beta parameter of the beta distribution.
+    #[derivative(Default(value = "1."))]
+    pub(super) beta: f32,
+    /// Documents in the [`Stack`](super::Stack).
+    pub(super) documents: Vec<Document>,
+}
+
+impl Data {
+    #[allow(dead_code)]
+    /// Create a `Data`.
+    pub(crate) fn new(alpha: f32, beta: f32, documents: Vec<Document>) -> Result<Self, Error> {
+        if alpha <= 0.0 {
+            return Err(Error::InvalidAlpha(alpha));
+        }
+        if beta <= 0.0 {
+            return Err(Error::InvalidBeta(beta));
+        }
+
+        Ok(Self {
+            alpha,
+            beta,
+            documents,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use claim::{assert_err, assert_matches, assert_ok};
+
+    use super::*;
+
+    #[test]
+    #[allow(clippy::float_cmp)]
+    fn test_stack_empty() {
+        let stack = Data::default();
+
+        assert_eq!(stack.alpha, 1.);
+        assert_eq!(stack.beta, 1.);
+        assert!(stack.documents.is_empty());
+    }
+
+    #[test]
+    #[allow(clippy::float_cmp)]
+    fn test_stack_from_parts() {
+        let stack = Data::new(0. + f32::EPSILON, 0. + f32::EPSILON, vec![]);
+        assert_ok!(stack);
+
+        let stack = Data::new(0.0, 0.5, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidAlpha(x) if x == 0.0);
+
+        let stack = Data::new(0.5, 0.0, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidBeta(x) if x == 0.0);
+
+        let stack = Data::new(-0.0, 1.0, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidAlpha(x) if x == 0.0);
+
+        let stack = Data::new(1.0, -0.0, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidBeta(x) if x == 0.0);
+
+        let stack = Data::new(-1.0, 1.0, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidAlpha(x) if x == -1.0);
+
+        let stack = Data::new(1.0, -1.0, vec![]);
+        assert_err!(&stack);
+        assert_matches!(stack.unwrap_err(), Error::InvalidBeta(x) if x == -1.0);
+    }
+}
