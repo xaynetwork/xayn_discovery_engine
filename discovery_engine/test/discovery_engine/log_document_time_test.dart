99c549509ef3a796d7042732c0fe16b775e6f2e2
Pawel Biernacki <pawel@robotygraficzne.com>
2021-12-17 17:19:37 +0100

    added tests for the logDocumentTime method

---
+++ discovery_engine/test/discovery_engine/log_document_time_test.dart
@@ -0,0 +1,81 @@
+import 'package:test/test.dart';
+import 'package:xayn_discovery_engine/discovery_engine.dart'
+    show
+        ClientEventSucceeded,
+        DocumentId,
+        EngineEvent,
+        EngineExceptionRaised,
+        EngineExceptionReason,
+        DocumentViewMode;
+
+import 'utils/utils.dart'
+    show
+        MockDiscoveryEngineWorker,
+        createEngineWithEntryPoint,
+        withSuccessResponse;
+
+void main() {
+  group('DiscoveryEngine logDocumentTime', () {
+    test(
+        'if worker responds with "ClientEventSucceeded" event it should pass it'
+        'as a response of the Discovery Engine', () async {
+      final engine = await createEngineWithEntryPoint(withSuccessResponse);
+
+      expect(
+        engine.logDocumentTime(
+          documentId: DocumentId(),
+          mode: DocumentViewMode.reader,
+          seconds: 10,
+        ),
+        completion(isA<ClientEventSucceeded>()),
+      );
+    });
+
+    test(
+        'if worker responds with "EngineExceptionRaised" event it should pass it'
+        'as a response of the Discovery Engine', () async {
+      final engine = await createEngineWithEntryPoint(withErrorResponse);
+      final response = await engine.logDocumentTime(
+        documentId: DocumentId(),
+        mode: DocumentViewMode.story,
+        seconds: 20,
+      );
+
+      expect(response, isA<EngineExceptionRaised>());
+      expect(
+        (response as EngineExceptionRaised).reason,
+        EngineExceptionReason.genericError,
+      );
+    });
+
+    test(
+        'if worker responds with something other than allowed events it should '
+        'catch it and respond with "EngineExceptionRaised" event '
+        'with "wrongEventInResponse" reason', () async {
+      final engine = await createEngineWithEntryPoint(withWrongEventResponse);
+      final response = await engine.logDocumentTime(
+        documentId: DocumentId(),
+        mode: DocumentViewMode.web,
+        seconds: 30,
+      );
+
+      expect(response, isA<EngineExceptionRaised>());
+      expect(
+        (response as EngineExceptionRaised).reason,
+        EngineExceptionReason.wrongEventInResponse,
+      );
+    });
+  });
+}
+
+void withErrorResponse(Object initialMessage) => MockDiscoveryEngineWorker(
+      initialMessage,
+      documentTimeLoggedResponse: const EngineEvent.engineExceptionRaised(
+        EngineExceptionReason.genericError,
+      ),
+    );
+
+void withWrongEventResponse(Object initialMessage) => MockDiscoveryEngineWorker(
+      initialMessage,
+      documentTimeLoggedResponse: const EngineEvent.nextFeedBatchAvailable(),
+    );
