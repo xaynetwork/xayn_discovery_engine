f7fd3a78a465d994cbdaa7681844016ec282e31a
Pawel Biernacki <pawel@robotygraficzne.com>
2022-01-04 19:51:26 +0100

    added DocumentViewMode and Duration type adapters

--- discovery_engine/lib/src/domain/models/view_mode.dart
+++ discovery_engine/lib/src/domain/models/view_mode.dart
@@ -1,6 +1,16 @@
+import 'package:hive/hive.dart';
+import 'package:xayn_discovery_engine/src/domain/repository/type_id.dart'
+    show documentViewModeTypeId;
+
+part 'view_mode.g.dart';
+
 /// Document viewer mode.
+@HiveType(typeId: documentViewModeTypeId)
 enum DocumentViewMode {
+  @HiveField(0)
   story,
+  @HiveField(1)
   reader,
+  @HiveField(2)
   web,
 }
