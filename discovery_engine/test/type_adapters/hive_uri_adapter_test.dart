1e9bb0bb4d7f7acddb74dbae29a69c2c56a3d815
Pawel Biernacki <pawel@robotygraficzne.com>
2022-01-04 19:23:35 +0100

    added adapter tests

---
+++ discovery_engine/test/type_adapters/hive_uri_adapter_test.dart
@@ -0,0 +1,42 @@
+import 'dart:io' show Directory;
+
+import 'package:hive/hive.dart' show Hive, Box;
+import 'package:test/test.dart';
+import 'package:xayn_discovery_engine/src/infrastructure/type_adapters/hive_uri_adapter.dart'
+    show UriAdapter;
+
+void main() {
+  group('HiveUriAdapter', () {
+    late Box<Uri> box;
+
+    setUpAll(() async {
+      Hive.registerAdapter(UriAdapter());
+      Hive.init(Directory.current.path);
+      box = await Hive.openBox<Uri>('HiveUriAdapter');
+    });
+
+    tearDown(() async {
+      await box.clear();
+    });
+
+    tearDownAll(() async {
+      await box.deleteFromDisk();
+    });
+
+    test('can write and read `Uri`', () async {
+      final value =
+          Uri.parse('http://example.com:8080/some/url?query=some query');
+      final key = await box.add(value);
+      final uri = box.get(key)!;
+
+      expect(box, hasLength(1));
+      expect(uri.scheme, equals('http'));
+      expect(uri.host, equals('example.com'));
+      expect(uri.path, equals('/some/url'));
+      expect(uri.pathSegments, equals(['some', 'url']));
+      expect(uri.port, equals(8080));
+      expect(uri.query, equals('query=some%20query'));
+      expect(uri.queryParameters, equals({'query': 'some query'}));
+    });
+  });
+}
