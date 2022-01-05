d814477eda02136fee55ae6a309760650d5cf528
Pawel Biernacki <pawel@robotygraficzne.com>
2022-01-05 09:52:49 +0100

    removed entrypoint which is not bad anymore

--- discovery_engine/test/discovery_engine/init_test.dart
+++ discovery_engine/test/discovery_engine/init_test.dart
@@ -1,5 +1,3 @@
-import 'dart:isolate' show SendPort;
-
 import 'package:test/test.dart';
 import 'package:xayn_discovery_engine/discovery_engine.dart'
     show
@@ -25,16 +23,11 @@ void main() {
     });
 
     test('when passing a bad entry point it should throw "EngineInitException"',
-        () {
+        () async {
       void wrongTypeSignature() {}
-      void nonStaticOrTopLevelFunction(SendPort sendPort) {}
 
       expect(
         createEngineWithEntryPoint(wrongTypeSignature),
-        throwsA(isA<EngineInitException>()),
-      );
-      expect(
-        createEngineWithEntryPoint(nonStaticOrTopLevelFunction),
         throwsA(isA<EngineInitException>()),
       );
     });
