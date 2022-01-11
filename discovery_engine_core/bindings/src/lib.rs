9239a67ef67249be56ebeb15eb0b53f3890478ed
Philipp Korber <philipp@korber.dev>
2022-01-12 00:44:03 +0100

    Added default lints to bindings/src/lib.rs.

--- discovery_engine_core/bindings/src/lib.rs
+++ discovery_engine_core/bindings/src/lib.rs
@@ -1,1 +1,15 @@
 // Copyright 2022 Xayn AG
+
+//! FFI and logic bindings to discovery_engine_core.
+
+#![deny(
+    clippy::pedantic,
+    clippy::future_not_send,
+    noop_method_call,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused_qualifications,
+    unsafe_op_in_unsafe_fn
+)]
+#![warn(missing_docs, unreachable_pub)]
+#![allow(clippy::must_use_candidate)]
