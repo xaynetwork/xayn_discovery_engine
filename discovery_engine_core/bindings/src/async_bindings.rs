bd72bcbf860a6223a15cee96c41fa8cab3293d69
Philipp Korber <philipp@korber.dev>
2022-01-12 01:36:07 +0100

    TY-2250 added async api to the bindings crate with a single "dummy" ffi function.

---
+++ discovery_engine_core/bindings/src/async_bindings.rs
@@ -0,0 +1,10 @@
+// Copyright 2022 Xayn AG
+
+
+//! Module containing auto-generated sub-modules with async-bindings.
+//!
+//! This exists as a workaround to cbindgen not being able to handle
+//! proc-macro generated code on stable.
+
+#[rustfmt::skip]
+pub mod async_core;
