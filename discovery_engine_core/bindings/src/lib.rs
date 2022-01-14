8444c0b75f7a7f6d5c109713ff51d7dc59f19831
Philipp Korber <philipp@korber.dev>
2022-01-14 13:41:46 +0100

    TY-2249 add `bindings` crate to discovery_engine_core (#48)
    * TY-2249 Added `bindings` crate to discovery_engine_core.
    
    * Added default lints to bindings/src/lib.rs.

---
+++ discovery_engine_core/bindings/src/lib.rs
@@ -0,0 +1,27 @@
+// Copyright 2022 Xayn AG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+//! FFI and logic bindings to `discovery_engine_core`.
+
+#![deny(
+    clippy::pedantic,
+    clippy::future_not_send,
+    noop_method_call,
+    rust_2018_idioms,
+    rust_2021_compatibility,
+    unused_qualifications,
+    unsafe_op_in_unsafe_fn
+)]
+#![warn(missing_docs, unreachable_pub)]
+#![allow(clippy::must_use_candidate)]
