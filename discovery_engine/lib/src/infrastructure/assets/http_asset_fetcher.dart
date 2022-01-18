6e19d57f3cd2fea8f678db4b3e2a4557824f9ea3
Pawel Biernacki <pawel@robotygraficzne.com>
2022-01-18 08:18:31 +0100

    added http_retry for TY-2275

--- discovery_engine/lib/src/infrastructure/assets/http_asset_fetcher.dart
+++ discovery_engine/lib/src/infrastructure/assets/http_asset_fetcher.dart
@@ -1,6 +1,7 @@
 import 'dart:typed_data' show Uint8List;
 
 import 'package:http/http.dart' as http;
+import 'package:http_retry/http_retry.dart' show RetryClient;
 import 'package:xayn_discovery_engine/src/domain/assets/asset_fetcher.dart'
     show AssetFetcher;
 import 'package:xayn_discovery_engine/src/domain/assets/data_provider.dart'
@@ -8,15 +9,17 @@ import 'package:xayn_discovery_engine/src/domain/assets/data_provider.dart'
 
 class HttpAssetFetcher extends AssetFetcher {
   final String _baseUrl;
-  final http.Client _client;
 
-  HttpAssetFetcher(this._baseUrl) : _client = http.Client();
+  HttpAssetFetcher(this._baseUrl);
 
   @override
   Future<Uint8List> fetch(String urlSuffix) async {
+    // TODO: maybe configure RetryClient
+    final client = RetryClient(http.Client());
+
     final url = DataProvider.joinPaths([_baseUrl, urlSuffix]);
     final uri = Uri.parse(url);
-    final response = await _client.get(uri);
+    final response = await client.get(uri);
 
     if (response.statusCode != 200) {
       // triggers when the asset is not available on the provided url
