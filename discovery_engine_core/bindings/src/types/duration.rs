a1f7246d2f74fbd031c6c0a5c55db35b6c08e995
Philipp Korber <philipp@korber.dev>
2022-01-31 12:28:10 +0100

    Fix license.

--- discovery_engine_core/bindings/src/types/duration.rs
+++ discovery_engine_core/bindings/src/types/duration.rs
@@ -1,3 +1,5 @@
+// Copyright 2022 Xayn AG
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, version 3.
