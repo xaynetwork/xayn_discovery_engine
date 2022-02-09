1e504a089b7f006851fbdbea834a47272da1ccc7
Philipp Korber <philipp@korber.dev>
2022-02-09 10:06:23 +0100

    Fixed imports.

--- discovery_engine/lib/src/ffi/types/uri.dart
+++ discovery_engine/lib/src/ffi/types/uri.dart
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import 'dart:ffi' show Pointer, Uint8;
+import 'dart:ffi' show Pointer;
 
 import 'package:xayn_discovery_engine/src/ffi/genesis.ffigen.dart'
     show RustUrl, RustOptionUrl;
