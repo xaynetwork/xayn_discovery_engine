2931eb9f2b585dc55650637cae8dfd1bf18f92df
Philipp Korber <philipp@korber.dev>
2022-02-14 22:00:43 +0100

    Added missing copyright header.

--- discovery_engine/test/ffi/types/box_test.dart
+++ discovery_engine/test/ffi/types/box_test.dart
@@ -1,4 +1,18 @@
-import 'dart:ffi';
+// Copyright 2022 Xayn AG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'dart:ffi' show Pointer, Uint32;
 
 import 'package:test/test.dart';
 import 'package:xayn_discovery_engine/src/ffi/types/box.dart' show Boxed;
