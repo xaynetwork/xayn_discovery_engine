e309b3e6f250bc5b469c3caf2f55a02aea9f69a9
janpetschexain <58227040+janpetschexain@users.noreply.github.com>
2022-02-17 09:11:11 +0000

    TY-2302 ffi engine dart (#142)
    **References**
    
    - [TY-2302]
      - [TY-2398]
      - [TY-2400]
      - [TY-2402]
      - [TY-2404]
      - [TY-2406]
      - [TY-2414]
    - follow up from #136
    - followed by #151
    
    **Summary**
    
    - impl dart ffi engine functions
    - fix dart ffi test
    - impl engine result ffi adapter
    - cleanup box ffi
    
    
    [TY-2302]: https://xainag.atlassian.net/browse/TY-2302?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2398]: https://xainag.atlassian.net/browse/TY-2398?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2400]: https://xainag.atlassian.net/browse/TY-2400?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2402]: https://xainag.atlassian.net/browse/TY-2402?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2404]: https://xainag.atlassian.net/browse/TY-2404?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2406]: https://xainag.atlassian.net/browse/TY-2406?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [TY-2414]: https://xainag.atlassian.net/browse/TY-2414?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ discovery_engine/lib/src/ffi/types/engine.dart
@@ -0,0 +1,170 @@
+// Copyright 2022 Xayn AG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import 'dart:ffi' show nullptr, Pointer;
+import 'dart:typed_data' show Uint8List;
+
+import 'package:xayn_discovery_engine/src/domain/models/active_data.dart'
+    show DocumentWithActiveData;
+import 'package:xayn_discovery_engine/src/domain/models/configuration.dart'
+    show Configuration;
+import 'package:xayn_discovery_engine/src/domain/models/feed_market.dart'
+    show FeedMarket;
+import 'package:xayn_discovery_engine/src/domain/models/time_spent.dart'
+    show TimeSpent;
+import 'package:xayn_discovery_engine/src/domain/models/user_reacted.dart'
+    show UserReacted;
+import 'package:xayn_discovery_engine/src/ffi/genesis.ffigen.dart'
+    show RustResultSharedEngineString, RustSharedEngine;
+import 'package:xayn_discovery_engine/src/ffi/load_lib.dart' show asyncFfi, ffi;
+import 'package:xayn_discovery_engine/src/ffi/types/box.dart' show Boxed;
+import 'package:xayn_discovery_engine/src/ffi/types/document/document_vec.dart'
+    show DocumentSliceFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/document/time_spent.dart'
+    show TimeSpentFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/document/user_reacted.dart'
+    show UserReactedFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/feed_market_vec.dart'
+    show FeedMarketSliceFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/init_config.dart'
+    show InitConfigFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/primitives.dart'
+    show Uint8ListFfi;
+import 'package:xayn_discovery_engine/src/ffi/types/result.dart'
+    show
+        resultSharedEngineStringFfiAdapter,
+        resultVecDocumentStringFfiAdapter,
+        resultVecU8StringFfiAdapter,
+        resultVoidStringFfiAdapter;
+import 'package:xayn_discovery_engine/src/infrastructure/assets/native/data_provider.dart'
+    show NativeSetupData;
+
+/// A handle to the discovery engine.
+class DiscoveryEngineFfi {
+  final Boxed<RustResultSharedEngineString> _boxedResult;
+  Pointer<RustSharedEngine> _sharedEngine;
+
+  DiscoveryEngineFfi._(final this._boxedResult, final this._sharedEngine);
+
+  /// Initializes the engine.
+  static Future<DiscoveryEngineFfi> initialize(
+    final Configuration config,
+    final NativeSetupData setupData, [
+    final Uint8List? state,
+  ]) async {
+    final boxedConfig = InitConfigFfi(config, setupData).allocNative();
+    final boxedState = state?.allocNative();
+
+    final boxedResult = Boxed(
+      await asyncFfi.initialize(
+        boxedConfig.move(),
+        boxedState?.move() ?? nullptr,
+      ),
+      ffi.drop_result_shared_engine_string,
+    );
+    final Pointer<RustSharedEngine> sharedEngine;
+    try {
+      sharedEngine = resultSharedEngineStringFfiAdapter.readNative(
+        boxedResult.mut,
+        mapErr: (error) => Exception(error),
+      );
+    } catch (_) {
+      boxedResult.free();
+      rethrow;
+    }
+
+    return DiscoveryEngineFfi._(boxedResult, sharedEngine);
+  }
+
+  /// Serializes the engine.
+  Future<Uint8List> serialize() async {
+    final boxedResult = Boxed(
+      await asyncFfi.serialize(_sharedEngine),
+      ffi.drop_result_vec_u8_string,
+    );
+
+    return resultVecU8StringFfiAdapter.consumeNative(
+      boxedResult,
+      mapErr: (error) => Exception(error),
+    );
+  }
+
+  /// Sets the markets.
+  Future<void> setMarkets(final List<FeedMarket> markets) async {
+    final boxedMarkets = markets.allocVec();
+    final boxedResult = Boxed(
+      await asyncFfi.setMarkets(_sharedEngine, boxedMarkets.move()),
+      ffi.drop_result_void_string,
+    );
+
+    return resultVoidStringFfiAdapter.consumeNative(
+      boxedResult,
+      mapErr: (error) => Exception(error),
+    );
+  }
+
+  /// Gets feed documents.
+  Future<List<DocumentWithActiveData>> getFeedDocuments(
+    final int maxDocuments,
+  ) async {
+    final boxedResult = Boxed(
+      await asyncFfi.getFeedDocuments(_sharedEngine, maxDocuments),
+      ffi.drop_result_vec_document_string,
+    );
+
+    return resultVecDocumentStringFfiAdapter
+        .consumeNative(
+          boxedResult,
+          mapErr: (error) => Exception(error),
+        )
+        .toDocumentListWithActiveData();
+  }
+
+  /// Processes time spent.
+  Future<void> timeSpent(final TimeSpent timeSpent) async {
+    final boxedTimeSpent = timeSpent.allocNative();
+    final boxedResult = Boxed(
+      await asyncFfi.timeSpent(_sharedEngine, boxedTimeSpent.move()),
+      ffi.drop_result_void_string,
+    );
+
+    return resultVoidStringFfiAdapter.consumeNative(
+      boxedResult,
+      mapErr: (error) => Exception(error),
+    );
+  }
+
+  /// Processes user reaction.
+  Future<void> userReacted(final UserReacted userReacted) async {
+    final boxedUserReacted = userReacted.allocNative();
+    final boxedResult = Boxed(
+      await asyncFfi.userReacted(_sharedEngine, boxedUserReacted.move()),
+      ffi.drop_result_void_string,
+    );
+
+    return resultVoidStringFfiAdapter.consumeNative(
+      boxedResult,
+      mapErr: (error) => Exception(error),
+    );
+  }
+
+  /// Drops the engine.
+  ///
+  /// # Safety
+  /// Must only be called after all other futures of the engine have been completed.
+  void free() {
+    _sharedEngine = nullptr;
+    _boxedResult.free();
+  }
+}
