5cd072e95db543853b2dc17e00d66dd407d3de8e
Andrea Corradi <andrea.corradi@xayn.com>
2022-02-22 16:47:54 +0000

    TY 2482 Return mock data for stacks (#175)
    Returns mock data from stack.
    This will be removed by #172.
    
    Modify the handling of `source_url` since `curl_url` is only a domain. This will be changed in another PR.

--- discovery_engine_core/core/src/stack/ops/personalized.rs
+++ discovery_engine_core/core/src/stack/ops/personalized.rs
@@ -15,11 +15,12 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use chrono::NaiveDate;
 use tokio::sync::RwLock;
 use uuid::Uuid;
 use xayn_ai::ranker::KeyPhrase;
 
-use xayn_discovery_engine_providers::{Article, Market};
+use xayn_discovery_engine_providers::{Article, Market, Topic};
 
 use crate::{
     document::Document,
@@ -39,15 +40,38 @@ pub(crate) struct PersonalizedNews {
 #[async_trait]
 impl Ops for PersonalizedNews {
     fn id(&self) -> Id {
-        Id(Uuid::parse_str("7861796e-0d0a-4a82-8054-193220aa63c6").unwrap(/* valid uuid */))
+        Id(Uuid::parse_str("311dc7eb-5fc7-4aa4-8232-e119f7e80e76").unwrap(/* valid uuid */))
     }
 
     fn configure(&mut self, config: &EndpointConfig) {
         self.markets.replace(Arc::clone(&config.markets));
     }
 
+    #[allow(clippy::cast_precision_loss)]
+    #[allow(clippy::cast_possible_truncation)]
     async fn new_items(&self, _key_phrases: &[KeyPhrase]) -> Result<Vec<Article>, GenericError> {
-        Ok(vec![])
+        let n = 10;
+        let articles = (0..n).fold(Vec::with_capacity(n), |mut articles, i| {
+            articles.push(
+            Article {
+                id: i.to_string(),
+                title: format!("P Document Title {}", i),
+                score: if i % 2 == 0 {Some(i as f32) } else {None},
+                rank: i,
+                clean_url: "xayn.com".to_string(),
+                excerpt: format!("Content of the news {}", i),
+                link: "https://xayn.com/".into(),
+                media: "https://uploads-ssl.webflow.com/5ea197660b956f76d26f0026/614349038d7d72d1576ae3f4_plant.svg".into(),
+                topic: Topic::Unrecognized,
+                country: "DE".to_string(),
+                language: "de".to_string(),
+                published_date: NaiveDate::from_ymd(2022, 2, (i + 1) as u32).and_hms(9, 10, 11),
+            });
+
+            articles
+        });
+
+        Ok(articles)
     }
 
     fn filter_articles(
