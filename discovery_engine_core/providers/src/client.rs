30efb10b6c9eae6c16597125702276f62115c24d
Philipp Korber <philipp@korber.dev>
2022-03-01 15:29:12 +0100

    fixup! TY-2516 reduce client timeout

--- discovery_engine_core/providers/src/client.rs
+++ discovery_engine_core/providers/src/client.rs
@@ -62,7 +62,7 @@ pub struct HeadlinesQuery<'a> {
 }
 
 impl Client {
-    const TIMEOUT: Duration = Duration::from_micros(350);
+    const TIMEOUT: Duration = Duration::from_millis(3500);
 
     /// Create a client.
     pub fn new(token: String, url: String) -> Self {
