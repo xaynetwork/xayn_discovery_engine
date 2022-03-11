26e9834895bb232f66226606f36137b9122f622a
Robert Debug <robertt.debug@gmail.com>
2022-03-11 15:36:25 +0100

    add tests

--- discovery_engine_core/core/src/stack/semantic_filtering.rs
+++ discovery_engine_core/core/src/stack/semantic_filtering.rs
@@ -1,15 +1,42 @@
+// Copyright 2022 Xayn AG
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use std::collections::BTreeMap;
 
+use displaydoc::Display;
 use kodama::{linkage, Dendrogram, Method};
+use thiserror::Error;
 use xayn_ai::ranker::cosine_distance;
 
 use crate::document::Document;
+
+/// Semantic clustering errors.
+#[derive(Error, Debug, Display)]
+pub enum Error {
+    /// No enough documents.
+    NotEnoughDocuments,
+}
 
 fn determine_semantic_clusters(
     documents: &[Document],
     method: Method,
     distance_threshold: f32,
-) -> Vec<usize> {
+) -> Result<Vec<usize>, Error> {
+    if documents.len() < 2 {
+        return Err(Error::NotEnoughDocuments);
+    }
+
     let length = (documents.len() * (documents.len() - 1)) / 2;
     let mut condensed_distance_matrix = Vec::with_capacity(length);
     for row in 0..documents.len() - 1 {
@@ -23,7 +50,8 @@ fn determine_semantic_clusters(
     }
 
     let dendrogram = linkage(&mut condensed_distance_matrix, documents.len(), method);
-    cut_tree(&dendrogram, distance_threshold)
+    let labels = cut_tree(&dendrogram, distance_threshold);
+    Ok(labels)
 }
 
 fn cut_tree(dendrogram: &Dendrogram<f32>, distance_threshold: f32) -> Vec<usize> {
@@ -59,4 +87,68 @@ fn cut_tree(dendrogram: &Dendrogram<f32>, distance_threshold: f32) -> Vec<usize>
             labels
         },
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cluster_no_enough_samples() {
+        let res = determine_semantic_clusters(&[], Method::Average, 1.);
+        assert!(matches!(res.unwrap_err(), Error::NotEnoughDocuments))
+    }
+
+    #[test]
+    fn test_cluster_one_documents() {
+        let res = determine_semantic_clusters(&[Document::default()], Method::Average, 1.);
+        assert!(matches!(res.unwrap_err(), Error::NotEnoughDocuments))
+    }
+
+    #[test]
+    fn test_cluster_multiple_documents() {
+        let res = determine_semantic_clusters(
+            &[Document::default(), Document::default()],
+            Method::Average,
+            1.,
+        );
+        assert_eq!(res.unwrap(), [0, 0])
+    }
+
+    #[test]
+    fn test_cut_tree_1_cluster() {
+        // cut ─────────────────────────
+        //         ┌────┴─────┐
+        //         │       ┌──┴─┐
+        //         |       |    |
+        //       ┌─┴──┐    │    │
+        //       A    B    C    D
+        let dendrogram = linkage(&mut [0.5, 3., 2., 3.5, 2.5, 1.], 4, Method::Single);
+        let labels = cut_tree(&dendrogram, 3.6);
+        assert_eq!(labels, [0, 0, 0, 0])
+    }
+
+    #[test]
+    fn test_cut_tree_3_cluster() {
+        //         ┌──────────┐
+        //         │       ┌──┴─┐
+        // cut ────┼───────┼────┼───────
+        //       ┌─┴──┐    │    │
+        //       A    B    C    D
+        let dendrogram = linkage(&mut [0.5, 3., 2., 3.5, 2.5, 1.], 4, Method::Single);
+        let labels = cut_tree(&dendrogram, 0.75);
+        assert_eq!(labels, [2, 2, 0, 1])
+    }
+
+    #[test]
+    fn test_cut_tree_4_cluster() {
+        //         ┌──────────┐
+        //         │       ┌──┴─┐
+        //         |       │    │
+        // cut ──┌─┼──┐────┼────┼───────
+        //       A    B    C    D
+        let dendrogram = linkage(&mut [0.5, 3., 2., 3.5, 2.5, 1.], 4, Method::Single);
+        let labels = cut_tree(&dendrogram, 0.5);
+        assert_eq!(labels, [0, 1, 2, 3])
+    }
 }
