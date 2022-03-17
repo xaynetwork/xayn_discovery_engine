ec17b63d351d681fd90604497a72a379ee14f1dc
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2022-03-17 13:57:41 +0100

    filter with combined distance

--- discovery_engine_core/core/src/stack/filters/semantic.rs
+++ discovery_engine_core/core/src/stack/filters/semantic.rs
@@ -21,12 +21,13 @@ use xayn_ai::ranker::pairwise_cosine_similarity;
 use crate::{document::Document, utils::nan_safe_f32_cmp};
 
 /// Agglomerates clusters wrt the documents' embeddings.
+#[allow(dead_code)]
 fn determine_semantic_clusters(documents: &[Document], distance_threshold: f32) -> Vec<usize> {
     if documents.len() < 2 {
         return vec![0; documents.len()];
     }
 
-    let mut condensed_dissimilarity_matrix = condensed_cosine_distance(documents);
+    let mut condensed_dissimilarity_matrix = condensed_cosine_similarity(documents);
     debug_assert_eq!(
         condensed_dissimilarity_matrix.len(),
         (documents.len() * (documents.len() - 1)) / 2,
@@ -40,19 +41,20 @@ fn determine_semantic_clusters(documents: &[Document], distance_threshold: f32) 
     cut_tree(&dendrogram, distance_threshold)
 }
 
-/// Computes the condensed cosine distance matrix of the documents' embeddings.
-fn condensed_cosine_distance(documents: &[Document]) -> Vec<f32> {
+/// Computes the condensed cosine similarity matrix of the documents' embeddings.
+fn condensed_cosine_similarity(documents: &[Document]) -> Vec<f32> {
     pairwise_cosine_similarity(
         documents
             .iter()
             .map(|document| document.smbert_embedding.view()),
     )
     .indexed_iter()
-    .filter_map(|((i, j), &similarity)| (i < j).then(|| 1. - similarity))
+    .filter_map(|((i, j), &similarity)| (i < j).then(|| similarity))
     .collect()
 }
 
 /// Agglomerates clusters wrt the documents' publication date differences.
+#[allow(dead_code)]
 fn determine_date_clusters(
     documents: &[Document],
     labels: &[usize],
@@ -131,28 +133,28 @@ fn condensed_date_distance(documents: &[Document], _cluster: &[usize]) -> Vec<f3
         .collect()
 }
 
-/// Computes the normalized condensed dissimilarity matrix of the documents' embeddings and dates.
-#[allow(dead_code)]
-fn condensed_distance(documents: &[Document], max_days: f32, threshold: f32) -> Vec<f32> {
-    let cosine_distance = condensed_cosine_distance(documents);
-    let date_distance = condensed_date_distance(documents, &[]);
-
+/// Computes the condensed decayed date distance matrix.
+fn condensed_decay_factor(
+    date_distance: Vec<f32>,
+    max_days: f32,
+    max_dissimilarity: f32,
+) -> Vec<f32> {
     let scale = 1. / ((-0.1 * max_days).exp() - 1.);
     let addend = scale * (-0.1 * max_days).exp();
-    let decay_factor = date_distance
+    date_distance
         .into_iter()
         .map(|distance| {
-            (scale * (threshold - 1.) * ((-0.1 * distance).exp() + addend)).max(0.) + threshold
+            (scale * (max_dissimilarity - 1.) * ((-0.1 * distance).exp() + addend)).max(0.)
+                + max_dissimilarity
         })
-        .collect::<Vec<_>>();
+        .collect()
+}
 
-    let dissimilarity = izip!(cosine_distance, decay_factor)
-        .map(|(distance, factor)| distance * factor)
+fn condensed_normalized_distance(cosine_similarity: Vec<f32>, decay_factor: Vec<f32>) -> Vec<f32> {
+    let combined = izip!(cosine_similarity, decay_factor)
+        .map(|(similarity, factor)| similarity * factor)
         .collect::<Vec<_>>();
-    debug_assert!(dissimilarity
-        .iter()
-        .all(|&dissimilarity| dissimilarity >= 0.));
-    let (min, max) = dissimilarity
+    let (min, max) = combined
         .iter()
         .copied()
         .minmax_by(nan_safe_f32_cmp)
@@ -161,22 +163,22 @@ fn condensed_distance(documents: &[Document], max_days: f32, threshold: f32) -> 
     let diff = max - min;
 
     if diff > 0. {
-        dissimilarity
+        combined
             .into_iter()
-            .map(|dis| (dis - min) / diff)
+            .map(|dis| 1. - ((dis - min) / diff))
             .collect()
     } else {
         // the denominator is zero iff either:
         // - there are less than two documents
         // - all documents have the same embedding and date
         // - all documents decayed because they are too old
-        // in either case all documents are treated as similar, ie with zero dissimilarity
-        vec![0.; dissimilarity.len()]
+        // in either case all documents are treated as similar, ie with minimum distance
+        vec![0.; combined.len()]
     }
 }
 
 /// Cuts off the dendrogram at the first step which exceeds the distance/dissimilarity threshold.
-fn cut_tree(dendrogram: &Dendrogram<f32>, threshold: f32) -> Vec<usize> {
+fn cut_tree(dendrogram: &Dendrogram<f32>, max_dissimilarity: f32) -> Vec<usize> {
     // at the beginning every sample is in its own cluster
     let clusters = (0..dendrogram.observations())
         .map(|x| (x, vec![x]))
@@ -186,7 +188,7 @@ fn cut_tree(dendrogram: &Dendrogram<f32>, threshold: f32) -> Vec<usize> {
     let (_, clusters) = dendrogram
         .steps()
         .iter()
-        .take_while(|step| step.dissimilarity < threshold)
+        .take_while(|step| step.dissimilarity < max_dissimilarity)
         .fold(
             (dendrogram.observations(), clusters),
             |(id, mut clusters), step| {
@@ -218,17 +220,17 @@ fn cut_tree(dendrogram: &Dendrogram<f32>, threshold: f32) -> Vec<usize> {
 
 /// Configurations for semantic filtering.
 pub(crate) struct SemanticFilterConfig {
-    /// Cluster cutoff threshold for dissimilarity of cosine distances.
-    distance_threshold: f32,
-    /// Cluster cutoff threshold for dissimilarity of date distances.
-    date_threshold: f32,
+    /// Maximum days threshold after which documents fully decay.
+    max_days: f32,
+    /// Cluster cutoff threshold for dissimilarity of normalized combined distances.
+    max_dissimilarity: f32,
 }
 
 impl Default for SemanticFilterConfig {
     fn default() -> Self {
         Self {
-            distance_threshold: 0.67,
-            date_threshold: 10.,
+            max_days: 10.,
+            max_dissimilarity: 0.67,
         }
     }
 }
@@ -238,8 +240,18 @@ pub(crate) fn filter_semantically(
     documents: Vec<Document>,
     config: &SemanticFilterConfig,
 ) -> Vec<Document> {
-    let labels = determine_semantic_clusters(&documents, config.distance_threshold);
-    let labels = determine_date_clusters(&documents, &labels, config.date_threshold);
+    if documents.len() < 2 {
+        return documents;
+    }
+
+    let cosine_similarity = condensed_cosine_similarity(&documents);
+    let date_distance = condensed_date_distance(&documents, &[]);
+    let decay_factor =
+        condensed_decay_factor(date_distance, config.max_days, config.max_dissimilarity);
+    let mut normalized_distance = condensed_normalized_distance(cosine_similarity, decay_factor);
+
+    let dendrogram = linkage(&mut normalized_distance, documents.len(), Method::Average);
+    let labels = cut_tree(&dendrogram, config.max_dissimilarity);
 
     izip!(documents, labels)
         .unique_by(|(_, label)| *label)
@@ -350,16 +362,30 @@ mod tests {
     }
 
     #[test]
+    fn test_filter_semantically_empty() {
+        let documents = vec![];
+        let config = SemanticFilterConfig::default();
+        let filtered = filter_semantically(documents, &config);
+        assert!(filtered.is_empty());
+    }
+
+    #[test]
+    fn test_filter_semantically_single() {
+        let documents = vec![Document::default()];
+        let config = SemanticFilterConfig::default();
+        let filtered = filter_semantically(documents.clone(), &config);
+        assert_eq!(filtered.len(), 1);
+        assert_eq!(filtered[0].id, documents[0].id);
+    }
+
+    #[test]
     fn test_filter_semantically_same() {
         let documents = vec![
             Document::default(),
             Document::default(),
             Document::default(),
         ];
-        let config = SemanticFilterConfig {
-            distance_threshold: 1.,
-            date_threshold: 1.,
-        };
+        let config = SemanticFilterConfig::default();
         let filtered = filter_semantically(documents.clone(), &config);
         assert_eq!(filtered.len(), 1);
         assert_eq!(filtered[0].id, documents[0].id);
@@ -373,8 +399,8 @@ mod tests {
             Document::default(),
         ];
         let config = SemanticFilterConfig {
-            distance_threshold: 0.,
-            date_threshold: 0.,
+            max_dissimilarity: 0.,
+            ..SemanticFilterConfig::default()
         };
         let filtered = filter_semantically(documents.clone(), &config);
         assert_eq!(filtered.len(), 3);
