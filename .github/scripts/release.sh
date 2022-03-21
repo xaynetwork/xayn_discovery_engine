973fc16024136fb8730efac897f441932bda3c3f
Alexandre Bargiela <alexandre.bargiela@xayn.com>
2022-03-21 11:17:05 +0100

    TY-2642 listing remote repo

--- .github/scripts/release.sh
+++ .github/scripts/release.sh
@@ -53,9 +53,6 @@ SRC_COMMIT_MSG=$(git log --format=%B -n1)
 
 # Check if the branch exists, if so, clone using the existing branch,
 # if not, clone using the default branch and let git push to send to the right branch
-echo "---- ssh-add list ----"
-ssh-add -l
-echo "---- ssh-add list ----"
 echo "---- git remote list ----"
 git remote -v
 echo "---- git remote list ----"
