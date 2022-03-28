302f9c6edd61efb8c83a776de3f9f8d1c202e0bb
Pawel Biernacki <pawel@robotygraficzne.com>
2022-03-28 12:28:40 +0200

    created a script

--- onnx-tool/src/mock_onnx_tool.py
+++ onnx-tool/src/mock_onnx_tool.py
@@ -1,122 +1,184 @@
 import os
+from argparse import ArgumentParser
 import onnx
 from onnx import helper, TensorProto
-import onnxruntime as rt
 
+def create_mock_onnx_model(model_path: str):
+    '''
+    Create a mock onnx model.
+    '''
 
-# Create one input (ValueInfoProto)
-input_type_proto = helper.make_tensor_type_proto(
-    TensorProto.INT64,
-    ['batch', 'sequence'],
-)
-inputs = [
-    helper.make_value_info('input_ids', input_type_proto),
-    helper.make_value_info('attention_mask', input_type_proto),
-    helper.make_value_info('token_type_ids', input_type_proto),
-]
+    # Create one input (ValueInfoProto)
+    input_type_proto = helper.make_tensor_type_proto(
+        TensorProto.INT64,
+        ['batch', 'sequence'],
+    )
+    inputs = [
+        helper.make_value_info('input_ids', input_type_proto),
+        helper.make_value_info('attention_mask', input_type_proto),
+        helper.make_value_info('token_type_ids', input_type_proto),
+    ]
 
-# Create one output (ValueInfoProto)
-outputs = [
-    helper.make_tensor_value_info(
-        'output_0',
+    # Create one output (ValueInfoProto)
+    outputs = [
+        helper.make_tensor_value_info(
+            'output_0',
+            TensorProto.FLOAT,
+            ['batch', 'sequence', 'Addoutput_0_dim_2'],
+        ),
+        helper.make_tensor_value_info(
+            'output_1',
+            TensorProto.FLOAT,
+            ['batch', 128],
+        ),
+    ]
+
+    # Create nodes (NodeProto)
+    const_tensor_value = helper.make_tensor(
+        name='const_tensor',
+        data_type=TensorProto.INT64,
+        dims=[1],
+        vals=[128],
+    )
+    condition_tensor = helper.make_tensor(
+        'condition_tensor',
+        TensorProto.BOOL,
+        [2],
+        [1, 0],
+    )
+    output_value = helper.make_tensor(
+        'output_value',
         TensorProto.FLOAT,
-        ['batch', 'sequence', 'Addoutput_0_dim_2'],
-    ),
-    helper.make_tensor_value_info(
-        'output_1',
-        TensorProto.FLOAT,
-        ['batch', 128],
-    ),
-]
+        [1],
+        [1],
+    )
+    nodes = [
+        helper.make_node(
+            'Constant',
+            inputs=[],
+            outputs=['const_tensor'],
+            value=const_tensor_value
+        ),
+        helper.make_node(
+            'Shape',
+            inputs=['input_ids'],
+            outputs=['input_ids_shape'],
+        ),
+        helper.make_node(
+            'Concat',
+            inputs=['input_ids_shape', 'const_tensor'],
+            outputs=['output_0_shape'],
+            axis=0,
+        ),
+        helper.make_node(
+            'Compress',
+            inputs=['input_ids_shape', 'condition_tensor'],
+            outputs=['output_1_batch_shape'],
+            axis=0,
+        ),
+        helper.make_node(
+            'Concat',
+            inputs=['output_1_batch_shape', 'const_tensor'],
+            outputs=['output_1_shape'],
+            axis=0,
+        ),
+        helper.make_node(
+            'ConstantOfShape',
+            inputs=['output_0_shape'],
+            outputs=['output_0'],
+            value=output_value,
+        ),
+        helper.make_node(
+            'ConstantOfShape',
+            inputs=['output_1_shape'],
+            outputs=['output_1'],
+            value=output_value,
+        ),
+        # helper.make_node(
+        #     'Expand',
+        #     inputs=['input_ids', 'output_0_shape'],
+        #     outputs=['output_0_int'],
+        # ),
+        # helper.make_node(
+        #     'Cast',
+        #     inputs=['output_0_int'],
+        #     outputs=['output_0'],
+        #     to=TensorProto.FLOAT,
+        # ),
+        # helper.make_node(
+        #     'Resize',
+        #     inputs=['input_ids', '', '', 'output_1_shape'],
+        #     outputs=['output_1_int'],
+        #     mode='nearest',
+        #     # allowzero=1,
+        # ),
+        # helper.make_node(
+        #     'Cast',
+        #     inputs=['output_1_int'],
+        #     outputs=['output_1'],
+        #     to=TensorProto.FLOAT,
+        # ),
+    ]
 
-# Create nodes (NodeProto)
-const_tensor_value = helper.make_tensor(
-    name='const_tensor',
-    data_type=TensorProto.INT64,
-    dims=[1],
-    vals=[128],
-)
-condition_tensor = helper.make_tensor(
-    'condition_tensor',
-    TensorProto.BOOL,
-    [2],
-    [1, 0],
-)
-output_value = helper.make_tensor(
-    'output_value',
-    TensorProto.FLOAT,
-    [1],
-    [1],
-)
-nodes = [
-    helper.make_node(
-        'Constant',
-        inputs=[],
-        outputs=['const_tensor'],
-        value=const_tensor_value
-    ),
-    helper.make_node(
-        'Shape',
-        inputs=['input_ids'],
-        outputs=['input_ids_shape'],
-    ),
-    helper.make_node(
-        'Concat',
-        inputs=['input_ids_shape', 'const_tensor'],
-        outputs=['output_0_shape'],
-        axis=0,
-    ),
-    helper.make_node(
-        'Compress',
-        inputs=['input_ids_shape', 'condition_tensor'],
-        outputs=['output_1_batch_shape'],
-        axis=0,
-    ),
-    helper.make_node(
-        'Concat',
-        inputs=['output_1_batch_shape', 'const_tensor'],
-        outputs=['output_1_shape'],
-        axis=0,
-    ),
-    helper.make_node(
-        'ConstantOfShape',
-        inputs=['output_0_shape'],
-        outputs=['output_0'],
-        value=output_value,
-    ),
-    helper.make_node(
-        'ConstantOfShape',
-        inputs=['output_1_shape'],
-        outputs=['output_1'],
-        value=output_value,
-    ),
-]
+    # Create the graph (GraphProto)
+    graph_def = helper.make_graph(
+        nodes,
+        'test-model',
+        inputs,
+        outputs,
+        [condition_tensor],
+    )
 
-# Create the graph (GraphProto)
-graph_def = helper.make_graph(
-    nodes,
-    'test-model',
-    inputs,
-    outputs,
-    [condition_tensor],
-)
+    # Create the model (ModelProto)
+    model_def = helper.make_model(
+        graph_def,
+        producer_name='ai.onnx',
+        opset_imports=[helper.make_opsetid('', 15)],
+    )
 
-# Create the model (ModelProto)
-model_def = helper.make_model(
-    graph_def,
-    producer_name='ai.onnx',
-    opset_imports=[helper.make_opsetid('', 15)],
-)
+    onnx.checker.check_model(model_def)
+    print(f"The model is checked: \N{heavy check mark}")
 
-onnx.checker.check_model(model_def)
-print('The model is checked!')
+    onnx.save(model_def, model_path)
+    print(f"The model is saved unde {model_path}: \N{heavy check mark}")
 
-model_path = os.path.join('src', 'smbert-mocked.onnx')
-model = onnx.save(model_def, model_path)
-print('The model is saved!\n')
+def verify(model_path: str):
+    '''
+    Verify the model.
+    '''
+    import onnxruntime as rt
 
-sess = rt.InferenceSession(model_path, providers=rt.get_available_providers())
-for inp in sess.get_inputs():
-    print("input name='{}' and shape={} and type={}".format(inp.name, inp.shape, inp.type))
-for out in sess.get_outputs():
-    print("output name='{}' and shape={} and type={}".format(out.name, out.shape, out.type))
+    print(f"Checking ONNX model loading from: {model_path} ...")
+    try:
+        sess = rt.InferenceSession(model_path, providers=rt.get_available_providers())
+        for inp in sess.get_inputs():
+            print("  input name='{}'\n    shape={}\n    type={}".format(inp.name, inp.shape, inp.type))
+        for out in sess.get_outputs():
+            print("  output name='{}'\n    shape={}\n    type={}".format(out.name, out.shape, out.type))
+        print(f"Model {model_path} correctly loaded: \N{heavy check mark}")
+    except Exception as re:
+        print(f"Error while loading the model {re}: \N{heavy ballot x}")
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--output",
+        type=str,
+        required=True,
+        help="Mock model's output path (ex: src/smbert-mocked.onnx)",
+    )
+    args = parser.parse_args()
+    model_path = os.path.abspath(args.output)
+    ext = model_path.split('.')[-1]
+
+    if ext != 'onnx':
+        print(f"The model file must have the extension .onnx: \N{heavy ballot x}")
+        exit(1)
+
+    try:
+        print("\n====== Converting model to ONNX ======")
+        create_mock_onnx_model(model_path)
+        verify(model_path)
+    except Exception as e:
+        print(f"Error while converting the model: {e}")
+        exit(1)
