9ead7a71384423a4c2d17ebb08a7f0fd8e20051f
Andrea Corradi <andrea.corradi@xayn.com>
2022-04-04 16:36:34 +0200

    Adapt path to assets

--- discovery_engine_core/xayn-ai/test-utils/src/asset.rs
+++ discovery_engine_core/xayn-ai/test-utils/src/asset.rs
@@ -9,13 +9,16 @@ use std::{
 use serde::Deserialize;
 use serde_json::from_reader;
 
-pub const DATA_DIR: &str = "data";
+pub const DATA_DIR: &str = "../discovery_engine_flutter/example/assets/";
 
 /// Resolves the path to the requested data relative to the workspace directory.
 pub fn resolve_path(path: &[impl AsRef<Path>]) -> Result<PathBuf> {
     let manifest = var_os("CARGO_MANIFEST_DIR")
         .ok_or_else(|| Error::new(ErrorKind::NotFound, "missing CARGO_MANIFEST_DIR"))?;
+
     let workspace = PathBuf::from(manifest)
+        .parent()
+        .ok_or_else(|| Error::new(ErrorKind::NotFound, "missing cargo workspace dir"))?
         .parent()
         .ok_or_else(|| Error::new(ErrorKind::NotFound, "missing cargo workspace dir"))?
         .to_path_buf();
@@ -34,22 +37,22 @@ struct Asset {
 
 #[derive(Deserialize)]
 struct Assets {
-    data_assets: Vec<Asset>,
+    assets: Vec<Asset>,
 }
 
 /// Reads the asset paths from the static assets file.
 fn read_assets() -> Result<HashMap<String, PathBuf>> {
-    from_reader::<_, Assets>(BufReader::new(File::open(resolve_path(&[
-        "assets_manifest.json",
-    ])?)?))
-    .map(|assets| {
-        assets
-            .data_assets
-            .into_iter()
-            .map(|asset| (asset.name, [DATA_DIR, &asset.url_suffix].iter().collect()))
-            .collect()
-    })
-    .map_err(|error| Error::new(ErrorKind::InvalidData, error.to_string()))
+    let path = resolve_path(&["../discovery_engine/lib/assets/asset_manifest.json"]);
+
+    from_reader::<_, Assets>(BufReader::new(File::open(path?)?))
+        .map(|assets| {
+            assets
+                .assets
+                .into_iter()
+                .map(|asset| (asset.name, [DATA_DIR, &asset.url_suffix].iter().collect()))
+                .collect()
+        })
+        .map_err(|error| Error::new(ErrorKind::InvalidData, error.to_string()))
 }
 
 /// Resolves the path to the requested asset relative to the workspace directory.
