d76af07f206c233f77f915d941d31c5717327e6e
Andrea Corradi <andrea.corradi@xayn.com>
2022-04-28 18:05:39 +0200

    Expose SearchBy enum

--- discovery_engine/lib/src/api/api.dart
+++ discovery_engine/lib/src/api/api.dart
@@ -68,7 +68,7 @@ export 'package:xayn_discovery_engine/src/api/events/engine_events.dart'
         EngineExceptionReason;
 export 'package:xayn_discovery_engine/src/api/models/document.dart';
 export 'package:xayn_discovery_engine/src/domain/models/active_search.dart'
-    show ActiveSearch;
+    show ActiveSearch, SearchBy;
 export 'package:xayn_discovery_engine/src/domain/models/configuration.dart';
 export 'package:xayn_discovery_engine/src/domain/models/document.dart'
     show UserReaction;
