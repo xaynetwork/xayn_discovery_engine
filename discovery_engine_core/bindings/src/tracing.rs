fbd7b1202179be7477cd0a9ff5d753b1a7677651
Philipp Korber <philipp@korber.dev>
2022-06-22 14:47:28 +0200

    Write to file if log_file is given and it is possible.

--- discovery_engine_core/bindings/src/tracing.rs
+++ discovery_engine_core/bindings/src/tracing.rs
@@ -14,22 +14,61 @@
 
 //! Setup tracing on different platform.
 
-use std::sync::Once;
+use std::{fs::OpenOptions, io, path::Path, sync::Once};
 
-use tracing_subscriber::{filter::LevelFilter, util::SubscriberInitExt};
+use tracing_subscriber::{filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 static INIT_TRACING: Once = Once::new();
 
-pub(crate) fn init_tracing() {
+pub(crate) fn init_tracing(log_file: Option<&Path>) {
     INIT_TRACING.call_once(|| {
-        init_tracing_once();
+        init_tracing_once(log_file);
+        init_panic_logging();
     });
 }
 
-fn init_tracing_once() {
-    let subscriber = tracing_subscriber::fmt()
-        .with_max_level(LevelFilter::INFO)
-        .finish();
+fn init_tracing_once(log_file: Option<&Path>) {
+    let max_level = LevelFilter::INFO;
+
+    let error = if let Some(log_file) = log_file {
+        match init_with_file_logging(max_level, log_file) {
+            Ok(()) => return,
+            Err(err) => Some(err),
+        }
+    } else {
+        None
+    };
+
+    init_normal_logging(max_level);
+
+    if let Some(error) = error {
+        tracing::error!("Initializing file logging faile: {}", error);
+    }
+}
+
+fn init_with_file_logging(max_level: LevelFilter, log_file: &Path) -> io::Result<()> {
+    let writer = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(log_file)?;
+
+    let layer = tracing_subscriber::fmt::layer()
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE)
+        .with_ansi(false)
+        .with_writer(writer);
+
+    tracing_subscriber::fmt()
+        .with_max_level(max_level)
+        .finish()
+        .with(layer)
+        .init();
+
+    Ok(())
+}
+
+fn init_normal_logging(max_level: LevelFilter) {
+    let subscriber = tracing_subscriber::fmt().with_max_level(max_level).finish();
 
     cfg_if::cfg_if! {
         if #[cfg(target_os = "android")] {
@@ -40,11 +79,9 @@ fn init_tracing_once() {
             subscriber.init();
         }
     }
-
-    log_panic();
 }
 
-fn log_panic() {
+fn init_panic_logging() {
     std::panic::set_hook(Box::new(|panic| {
         if let Some(location) = panic.location() {
             tracing::error!(
