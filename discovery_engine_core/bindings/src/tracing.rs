f8031d2e3d1154cdcd4d12d26b5ad24162a4b17c
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2022-06-28 13:15:07 +0200

    fix typo

--- discovery_engine_core/bindings/src/tracing.rs
+++ discovery_engine_core/bindings/src/tracing.rs
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-//! Setup tracing on different platform.
+//! Setup tracing on different platforms.
 
 use std::{fs::OpenOptions, io, path::Path, sync::Once};
 
