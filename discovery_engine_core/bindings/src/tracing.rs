3152c972b500b922f03f0e888981bc8520b0d577
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2022-06-28 10:31:57 +0200

    replace env filter with level filter

--- discovery_engine_core/bindings/src/tracing.rs
+++ discovery_engine_core/bindings/src/tracing.rs
@@ -14,10 +14,9 @@
 
 //! Setup tracing on different platform.
 
-use std::{env::VarError, error::Error, fs::OpenOptions, io, path::Path, sync::Once};
+use std::{fs::OpenOptions, io, path::Path, sync::Once};
 
-use tracing::metadata::LevelFilter;
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
+use tracing_subscriber::{filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 static INIT_TRACING: Once = Once::new();
 
@@ -29,8 +28,6 @@ pub(crate) fn init_tracing(log_file: Option<&Path>) {
 }
 
 fn init_tracing_once(log_file: Option<&Path>) {
-    let mut delayed_errors = Vec::new();
-
     let stdout_log = tracing_subscriber::fmt::layer();
 
     let subscriber = tracing_subscriber::registry();
@@ -53,60 +50,22 @@ fn init_tracing_once(log_file: Option<&Path>) {
             Ok(tracing_subscriber::fmt::layer().with_writer(writer).json())
         })
         .transpose()
-        .map_err(|err| {
-            delayed_errors.push(Box::new(err) as _);
+        .map_err(|error| {
+            tracing::error!(%error, "logging setup failed");
         })
         .ok();
 
-    let filter = build_env_filter(&mut delayed_errors);
+    let level = if log_file.is_some() {
+        LevelFilter::DEBUG
+    } else {
+        LevelFilter::INFO
+    };
 
     subscriber
         .with(stdout_log)
         .with(file_log)
-        .with(filter)
+        .with(level)
         .init();
-
-    for error in delayed_errors {
-        tracing::error!(%error, "logging setup failed");
-    }
-}
-
-/// Creates an [`EnvFilter`] based on the env of `RUST_LOG`.
-///
-/// If `RUST_LOG` is not set/empty `DISCOVERY_ENGINE_LOG` will
-/// be used instead in the form of `info,xayn_=${DISCOVERY_ENGINE_LOG}`,
-/// if that fails `info` is used.
-fn build_env_filter(errors: &mut Vec<Box<dyn Error>>) -> EnvFilter {
-    if let Some(directives) = env_var("RUST_LOG", errors) {
-        match directives.parse() {
-            Ok(val) => return val,
-            Err(err) => {
-                errors.push(Box::new(err));
-            }
-        }
-    }
-
-    if let Some(de_level) = env_var("DISCOVERY_ENGINE_LOG", errors) {
-        match format!("info,xayn_={}", de_level).parse() {
-            Ok(val) => return val,
-            Err(err) => {
-                errors.push(Box::new(err));
-            }
-        }
-    }
-
-    EnvFilter::default().add_directive(LevelFilter::INFO.into())
-}
-
-fn env_var(var: &str, errors: &mut Vec<Box<dyn Error>>) -> Option<String> {
-    match std::env::var(var) {
-        Ok(var) if !var.trim().is_empty() => return Some(var),
-        Err(err @ VarError::NotUnicode(_)) => {
-            errors.push(Box::new(err));
-        }
-        _ => (),
-    }
-    None
 }
 
 fn init_panic_logging() {
