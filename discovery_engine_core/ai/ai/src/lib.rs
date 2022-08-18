d2a7c04b6cba5b36259771d030d4d870f31e004f
Frank Pepermans <frank@igindo.com>
2022-08-18 11:41:11 +0200

    move triangular into utils

--- discovery_engine_core/ai/ai/src/lib.rs
+++ discovery_engine_core/ai/ai/src/lib.rs
@@ -48,11 +48,10 @@ pub use crate::{
     },
     document::{Document, DocumentId},
     embedding::{
-        cosine_similarity, l2_norm, pairwise_cosine_similarity, triangular_product, Embedding,
-        MalformedBytesEmbedding,
+        cosine_similarity, l2_norm, pairwise_cosine_similarity, Embedding, MalformedBytesEmbedding,
     },
     error::GenericError,
-    utils::{nan_safe_f32_cmp, nan_safe_f32_cmp_desc},
+    utils::{nan_safe_f32_cmp, nan_safe_f32_cmp_desc, triangular_product},
 };
 
 #[cfg(doc)]
