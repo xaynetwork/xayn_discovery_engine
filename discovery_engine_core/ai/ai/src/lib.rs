4b5d9cc4ce824b6ae22a29c15feb543d42e6b03b
Frank Pepermans <frank@igindo.com>
2022-08-20 20:32:04 +0200

    fmt?

--- discovery_engine_core/ai/ai/src/lib.rs
+++ discovery_engine_core/ai/ai/src/lib.rs
@@ -48,7 +48,11 @@ pub use crate::{
     },
     document::{Document, DocumentId},
     embedding::{
-        cosine_similarity, l2_norm, pairwise_cosine_similarity, Embedding, MalformedBytesEmbedding,
+        cosine_similarity,
+        l2_norm,
+        pairwise_cosine_similarity,
+        Embedding,
+        MalformedBytesEmbedding,
     },
     error::GenericError,
     utils::{nan_safe_f32_cmp, nan_safe_f32_cmp_desc, triangular_product},
