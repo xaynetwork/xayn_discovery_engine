6fbacc0ff0facb7866a84cd94d73d01f4f3560df
Pawel Biernacki <pawel@robotygraficzne.com>
2022-09-19 16:25:08 +0200

    cleanup after rebase

--- discovery_engine_core/web-api/src/handlers.rs
+++ discovery_engine_core/web-api/src/handlers.rs
@@ -49,15 +49,6 @@ pub(crate) async fn handle_user_interaction(
     db: Db,
 ) -> Result<impl warp::Reply, Rejection> {
     if let Some(document) = db.documents_by_id.get(&body.document_id) {
-        let mut user_interests = db
-            .user_state
-            .fetch(&user_id)
-            .await
-            .map_err(handle_user_state_op_error)?;
-
-        db.coi
-            .log_positive_user_reaction(&mut user_interests.positive, &document.smbert_embedding);
-
         db.user_state
             .update_positive_cois(&user_id, |positive_cois| {
                 db.coi
