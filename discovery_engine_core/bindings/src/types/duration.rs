58e84d66debee9240e9b7a0adc7e492cbcd923f0
Philipp Korber <philipp@korber.dev>
2022-09-20 02:46:47 +0200

    TO-3104-2 migrate documents, ffi parts only

--- discovery_engine_core/bindings/src/types/duration.rs
+++ discovery_engine_core/bindings/src/types/duration.rs
@@ -28,14 +28,61 @@ pub unsafe extern "C" fn init_duration_at(place: *mut Duration, seconds: u64, na
     }
 }
 
+/// Initializes a [`Option<Duration>`] field at given place to `Some` value.
+///
+/// # Safety
+///
+/// It must be valid to write a [`Option<Duration>`] to given pointer.
+#[no_mangle]
+pub unsafe extern "C" fn init_option_duration_some_at(
+    place: *mut Option<Duration>,
+    seconds: u64,
+    nanos: u32,
+) {
+    let value = Some(Duration::new(seconds, nanos));
+    unsafe {
+        place.write(value);
+    }
+}
+
+/// Initializes a [`Option<Duration>`] field at given place to `None`.
+///
+/// # Safety
+///
+/// It must be valid to write a [`Option<Duration>`] to given pointer.
+#[no_mangle]
+pub unsafe extern "C" fn init_option_duration_none_at(place: *mut Option<Duration>) {
+    unsafe {
+        place.write(None);
+    }
+}
+
 /// Gets the seconds of a duration at given place.
 ///
 /// # Safety
 ///
 /// The pointer must point to a valid [`Duration`] instance.
 #[no_mangle]
-pub unsafe extern "C" fn get_duration_seconds(duration: *mut Duration) -> u64 {
-    unsafe { &*duration }.as_secs()
+pub extern "C" fn get_duration_seconds(duration: &Duration) -> u64 {
+    duration.as_secs()
+}
+
+/// Returns true if given [`Option<Duration>`] is some.
+///
+/// Due to limitations of darts `ffigen` tool the `bool` is cast to `u8`.
+#[no_mangle]
+pub extern "C" fn get_option_duration_is_some(duration: &Option<Duration>) -> u8 {
+    duration.is_some().into()
+}
+
+/// Gets the seconds of a optional duration at given place.
+///
+/// If the option is `None` the value `0` is returned, use
+/// [`get_option_duration_is_some()`] to differentiate between a
+/// `0`-duration and no duration.
+#[no_mangle]
+pub extern "C" fn get_option_duration_seconds(duration: &Option<Duration>) -> u64 {
+    duration.as_ref().map_or(0, Duration::as_secs)
 }
 
 /// Gets the (subseconds) nanoseconds of a duration at given place.
@@ -46,6 +93,16 @@ pub unsafe extern "C" fn get_duration_seconds(duration: *mut Duration) -> u64 {
 #[no_mangle]
 pub unsafe extern "C" fn get_duration_nanos(duration: *mut Duration) -> u32 {
     unsafe { &*duration }.subsec_nanos()
+}
+
+/// Gets the sub-second nano seconds of a optional duration at given place.
+///
+/// If the option is `None` the value `0` is returned, use
+/// [`get_option_duration_is_some()`] to differentiate between a
+/// `0`-duration and no duration.
+#[no_mangle]
+pub extern "C" fn get_option_duration_nanos(duration: &Option<Duration>) -> u32 {
+    duration.as_ref().map_or(0, Duration::subsec_nanos)
 }
 
 /// Alloc an uninitialized `Box<Duration>`, mainly used for testing.
@@ -87,5 +144,32 @@ mod tests {
             init_duration_at(place, duration.as_secs(), duration.subsec_nanos());
         }
         assert_eq!(duration, *place);
+    }
+
+    #[test]
+    fn test_init_option_duration() {
+        let duration = Duration::new(78978, 89378);
+        //ok as Duration: !Drop
+        let place = &mut None;
+        unsafe {
+            init_option_duration_some_at(place, duration.as_secs(), duration.subsec_nanos());
+        }
+        assert_eq!(*place, Some(duration));
+        unsafe {
+            init_option_duration_none_at(place);
+        }
+        assert_eq!(*place, None);
+    }
+
+    #[test]
+    fn test_get_option_duration() {
+        let place = &mut None;
+        assert_eq!(get_option_duration_is_some(place), 0);
+        assert_eq!(get_option_duration_seconds(place), 0);
+        assert_eq!(get_option_duration_nanos(place), 0);
+        *place = Some(Duration::new(78978, 89378));
+        assert_eq!(get_option_duration_is_some(place), 1);
+        assert_eq!(get_option_duration_seconds(place), 78978);
+        assert_eq!(get_option_duration_nanos(place), 89378);
     }
 }
