ce9f20f0685f703c334a00528f72ed262f7a0518
Kwok Cheung <kwok.doc@gmail.com>
2022-09-21 15:09:24 +0200

    typo trailing comma in sql script

--- discovery_engine_core/core/src/storage/migrations/20220823100617_source_reaction.sql
+++ discovery_engine_core/core/src/storage/migrations/20220823100617_source_reaction.sql
@@ -16,5 +16,5 @@ CREATE TABLE IF NOT EXISTS SourceReaction (
     source TEXT NOT NULL PRIMARY KEY,
     weight INTEGER NOT NULL,
     -- format should be RFC3339/ISO8601 & sqlite compliant
-    lastUpdated TEXT NOT NULL,
+    lastUpdated TEXT NOT NULL
 );
