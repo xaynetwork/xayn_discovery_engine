91651358bd87abe36268063b0b3230d9569688db
maciejkrajewski <maciejkrajewskiml@gmail.com>
2022-09-29 11:20:48 +0200

    conflict merge fix

--- discovery_engine_core/web-api/src/elastic.rs
+++ discovery_engine_core/web-api/src/elastic.rs
@@ -12,6 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use itertools::Itertools;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
