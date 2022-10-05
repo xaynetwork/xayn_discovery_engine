ed9a707b631114d6b895259cecbcbceca35ca22e
Philipp Korber <philipp@korber.dev>
2022-10-05 13:35:28 +0200

    fix

--- discovery_engine_core/bindings/src/lib.rs
+++ discovery_engine_core/bindings/src/lib.rs
@@ -505,7 +505,7 @@ impl XaynDiscoveryEngineAsyncFfi {
 
     /// Disposes the engine.
     pub async fn dispose(engine: Box<SharedEngine>) {
-    drop(engine.as_ref().as_ref().lock().await);
+        drop(engine.as_ref().as_ref().lock().await);
     }
 
     /// Reset the AI state of this engine
