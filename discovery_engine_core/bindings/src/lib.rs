54802af8dd35b226ff96c0e0dec3f6c7f3e456ed
Philipp Korber <philipp@korber.dev>
2022-10-05 12:23:20 +0200

    Fix clippy error.
    For some reason the error isn't linted in all situations.

--- discovery_engine_core/bindings/src/lib.rs
+++ discovery_engine_core/bindings/src/lib.rs
@@ -505,7 +505,7 @@ impl XaynDiscoveryEngineAsyncFfi {
 
     /// Disposes the engine.
     pub async fn dispose(engine: Box<SharedEngine>) {
-        engine.as_ref().as_ref().lock().await;
+        drop(engine);
     }
 
     /// Reset the AI state of this engine
