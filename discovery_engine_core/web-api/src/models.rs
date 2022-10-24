1282b5add2681b163bc7bbebf5dd4b4ccb6ba584
Michael <michael.k.malak@gmail.com>
2022-10-24 17:28:14 +0000

    ET-3461: url encode id path (#658)
    **References**
    
    - [ET-3461]
    
    **Summary**
    
    All the `DocumentId` and `DocumentPropertyId` inputs in the URL of a request to ElasticSearch is now urlencoded.
    
    [ET-3461]: https://xainag.atlassian.net/browse/ET-3461

--- discovery_engine_core/web-api/src/models.rs
+++ discovery_engine_core/web-api/src/models.rs
@@ -12,7 +12,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use std::{collections::HashMap, ops::RangeInclusive, string::FromUtf8Error};
+use std::{borrow::Cow, collections::HashMap, ops::RangeInclusive, string::FromUtf8Error};
 
 use derive_more::{AsRef, Display};
 use displaydoc::Display as DisplayDoc;
@@ -106,9 +106,13 @@ impl DocumentId {
             Ok(Self(id))
         }
     }
+
+    pub fn encode(&self) -> Cow<str> {
+        urlencoding::encode(self.as_ref())
+    }
 }
 
-#[derive(Clone, Debug, Display, Serialize, Deserialize, PartialEq, Eq, Hash)]
+#[derive(Clone, Debug, Display, Serialize, Deserialize, PartialEq, Eq, Hash, AsRef)]
 pub struct DocumentPropertyId(String);
 
 impl DocumentPropertyId {
@@ -122,6 +126,10 @@ impl DocumentPropertyId {
         } else {
             Ok(Self(id))
         }
+    }
+
+    pub fn encode(&self) -> Cow<str> {
+        urlencoding::encode(self.as_ref())
     }
 }
 
