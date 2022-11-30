1e1017a5e07903e20d6945e414b0d485172eb767
PanicButtonPressed <felix.reichel@xayn.com>
2022-11-30 16:33:35 +0100

    add ndcg and user loop

--- web-api/src/mind.rs
+++ web-api/src/mind.rs
@@ -14,9 +14,10 @@
 #![allow(dead_code)]
 
 //! Executes the user-based MIND benchmark.
-use std::{collections::HashMap, fs::File, path::Path};
+use std::{cmp::min, collections::HashMap, fs::File, path::Path};
 
 use csv::{DeserializeRecordsIntoIter, Reader, ReaderBuilder};
+use rand::{seq::SliceRandom, thread_rng};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
@@ -68,15 +69,56 @@ fn run_benchmark() -> Result<(), anyhow::Error> {
     // and rerank the news in an impression
     for impression in impressions_iter {
         let impression: Impression = impression?;
-        let clicks = impression.clicks.split(' ').collect::<Vec<&str>>();
-        match articles.get(clicks[0]) {
-            Some(imp) => println!("{:?}", imp),
-            None => println!("Article id {} not found.", clicks[0]),
-        }
+        let clicks_iter = impression.clicks.split(' ');
 
+        // Placeholder for interacting with the entire click history
+        clicks_iter.for_each(|click| match articles.get(click) {
+            Some(article) => println!("The article {:?} was interacted.", article),
+            None => println!("Article id {} not found.", click),
+        });
+
+        let news = impression.news.split(' ').collect::<Vec<&str>>();
+
+        // Placeholder for reranking the results
+        let mut news_ids_labels = news
+            .iter()
+            .map(|x| x.split('-').collect::<Vec<_>>())
+            .collect::<Vec<Vec<_>>>();
+        news_ids_labels.shuffle(&mut thread_rng());
+
+        let labels = news_ids_labels
+            .iter()
+            .map(|x| x[1].parse::<f32>().unwrap())
+            .collect::<Vec<_>>();
+        let ndcgs = ndcg(&labels[..], &[3]);
+        println!("{:?}", ndcgs);
     }
 
     Ok(())
+}
+
+fn ndcg(relevance: &[f32], k: &[usize]) -> Vec<f32> {
+    let mut optimal_order = relevance.to_owned();
+    optimal_order.sort_by(|a, b| b.partial_cmp(a).unwrap());
+
+    let last = min(*k.iter().max().unwrap() as usize, relevance.len());
+
+    let mut out = Vec::new();
+    let mut dcg: f32 = 0.0;
+    let mut ideal_dcg: f32 = 0.0;
+
+    #[allow(clippy::cast_precision_loss)] // small numbers
+    for i in 0..last {
+        dcg += (2f32.powf(relevance[i]) - 1.0) / (i as f32 + 2.0).log2();
+        ideal_dcg += (2f32.powf(optimal_order[i]) - 1.0) / ((i as f32) + 2.0).log2();
+
+        out.push(dcg / (ideal_dcg + 0.00001));
+    }
+    out.into_iter()
+        .enumerate()
+        .filter(|&(i, _)| k.contains(&(i + 1)))
+        .map(|(_, e)| e)
+        .collect::<Vec<f32>>()
 }
 
 fn main() {
