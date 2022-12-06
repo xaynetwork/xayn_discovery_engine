a507f0579cb930829660c84dd51e3633c825ab9e
Felix Reichel <felix.reichel@xayn.com>
2022-12-06 14:26:38 +0000

    ET-3592 user related test workflow (#719)
    **Reference**
    
    - [ET-3592](https://xainag.atlassian.net/browse/ET-3592)
    
    **Summary**
    
    Add the user-related workflow by lookup for the snippets according to news id, add placeholders for reranking logic and calculate NDCG.

--- web-api/src/mind.rs
+++ web-api/src/mind.rs
@@ -17,7 +17,10 @@
 use std::{collections::HashMap, fs::File, path::Path};
 
 use csv::{DeserializeRecordsIntoIter, Reader, ReaderBuilder};
+use ndarray::{Array, ArrayView};
+use rand::{seq::SliceRandom, thread_rng};
 use serde::Deserialize;
+use xayn_ai_coi::nan_safe_f32_cmp_desc;
 
 #[derive(Debug, Deserialize)]
 struct Impression {
@@ -37,6 +40,8 @@ struct Article {
     snippet: String,
     url: String,
 }
+
+struct SnippetLabelPair(String, String);
 
 fn read<T>(path: &str) -> Result<DeserializeRecordsIntoIter<File, T>, anyhow::Error>
 where
@@ -62,18 +67,78 @@ fn run_benchmark() -> Result<(), anyhow::Error> {
         .map(|result| result.map(|article: Article| (article.id.clone(), article)))
         .collect::<Result<HashMap<_, _>, _>>()?;
 
-    let impressions_iter = read("behaviors.tsv")?;
+    let impressions = read("behaviors.tsv")?;
 
-    for impression in impressions_iter {
+    let nranks = vec![3];
+    let mut ndcgs = Array::zeros((nranks.len(), 0));
+
+    // Loop over all impressions, prepare reranker with news in click history
+    // and rerank the news in an impression
+    for impression in impressions {
         let impression: Impression = impression?;
-        let clicks = impression.clicks.split(' ').collect::<Vec<&str>>();
-        match articles.get(clicks[0]) {
-            Some(imp) => println!("{:?}", imp),
-            None => println!("Article id {} not found.", clicks[0]),
+
+        // Placeholder for interacting with the entire click history
+        for click in impression.clicks.split(' ') {
+            match articles.get(click) {
+                Some(article) => println!("The article {:?} was interacted.", article),
+                None => println!("Article id {} not found.", click),
+            }
         }
+
+        // Placeholder for reranking the results
+        let mut snippet_label_pairs = impression
+            .news
+            .split(' ')
+            .filter_map(|x| {
+                x.split_once('-').and_then(|(id, label)| {
+                    articles.get(id).map(|article| {
+                        SnippetLabelPair(article.snippet.to_string(), label.to_string())
+                    })
+                })
+            })
+            .collect::<Vec<_>>();
+        snippet_label_pairs.shuffle(&mut thread_rng());
+
+        let labels = snippet_label_pairs
+            .iter()
+            .map(|snippet_label| snippet_label.1.parse::<f32>().unwrap())
+            .collect::<Vec<_>>();
+        let ndcgs_iteration = ndcg(&labels, &nranks);
+
+        ndcgs
+            .push_column(ArrayView::from(&ndcgs_iteration))
+            .unwrap();
     }
+    println!("{:?}", ndcgs);
 
     Ok(())
+}
+
+fn ndcg(relevance: &[f32], k: &[usize]) -> Vec<f32> {
+    let mut optimal_order = relevance.to_owned();
+    optimal_order.sort_by(nan_safe_f32_cmp_desc);
+    let last = k
+        .iter()
+        .max()
+        .copied()
+        .map_or_else(|| relevance.len(), |k| k.min(relevance.len()));
+    relevance
+        .iter()
+        .zip(optimal_order)
+        .take(last)
+        .scan(
+            (1_f32, 0., 0.),
+            |(i, dcg, ideal_dcg), (relevance, optimal_order)| {
+                *i += 1.;
+                let log_i = (*i).log2();
+                *dcg += (2_f32.powf(*relevance) - 1.) / log_i;
+                *ideal_dcg += (2_f32.powf(optimal_order) - 1.) / log_i;
+                Some(*dcg / (*ideal_dcg + 0.00001))
+            },
+        )
+        .enumerate()
+        .filter_map(|(i, ndcg)| k.contains(&(i + 1)).then_some(ndcg))
+        .collect()
 }
 
 fn main() {
