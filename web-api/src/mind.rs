6e04d97d75a7fe9fba129694e3b4aeee81015313
maciejkrajewski <maciejkrajewskiml@gmail.com>
2022-12-08 18:10:01 +0100

    adds proper reading from the files

--- web-api/src/mind.rs
+++ web-api/src/mind.rs
@@ -19,28 +19,84 @@ use std::{collections::HashMap, fs::File, path::Path};
 use csv::{DeserializeRecordsIntoIter, Reader, ReaderBuilder};
 use itertools::Itertools;
 use ndarray::{Array, ArrayView};
-use rand::{seq::IteratorRandom, seq::SliceRandom, thread_rng};
-use rand::{seq::IteratorRandom, thread_rng};
-use rand::{seq::SliceRandom, thread_rng};
 use rand::{
     seq::{IteratorRandom, SliceRandom},
     thread_rng,
 };
-use serde::Deserialize;
+use serde::{de, Deserialize, Deserializer};
 use xayn_ai_coi::nan_safe_f32_cmp_desc;
+
+use crate::models::DocumentId;
+
+#[derive(Debug, Deserialize)]
+struct ViewedDocument {
+    document_id: DocumentId,
+    was_clicked: bool,
+}
+
+impl ViewedDocument {
+    fn new(document_id: DocumentId, was_clicked: bool) -> Self {
+        Self {
+            document_id,
+            was_clicked,
+        }
+    }
+}
+
+fn deserialize_viewed_documents<'de, D>(deserializer: D) -> Result<Vec<ViewedDocument>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let viewed_documents = s.split(' ');
+    viewed_documents
+        .map(|viewed_document| -> Result<_, D::Error> {
+            let mut parts = viewed_document.split('-');
+            let document_id = parts
+                .next()
+                .ok_or_else(|| de::Error::custom("missing document id"))?;
+            let document_id = DocumentId::new(document_id).map_err(de::Error::custom)?;
+            let was_clicked = parts
+                .next()
+                .ok_or_else(|| de::Error::custom("missing was_clicked"))?;
+            let was_clicked = match was_clicked {
+                "0" => Ok(false),
+                "1" => Ok(true),
+                _ => Err(de::Error::custom("invalid was_clicked")),
+            }?;
+            Ok(ViewedDocument::new(document_id, was_clicked))
+        })
+        .collect()
+}
+
+fn deserialize_clicked_documents<'de, D>(deserializer: D) -> Result<Vec<DocumentId>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = String::deserialize(deserializer)?;
+    let viewed_documents = s.split(' ');
+    viewed_documents
+        .map(|document| -> Result<_, D::Error> {
+            let document_id = DocumentId::new(document).map_err(de::Error::custom)?;
+            Ok(document_id)
+        })
+        .collect()
+}
 
 #[derive(Debug, Deserialize)]
 struct Impression {
     id: String,
     user_id: String,
     time: String,
-    clicks: String,
-    news: String,
+    #[serde(deserialize_with = "deserialize_clicked_documents")]
+    clicks: Vec<DocumentId>,
+    #[serde(deserialize_with = "deserialize_viewed_documents")]
+    news: Vec<ViewedDocument>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-struct Article {
-    id: String,
+struct Document {
+    id: DocumentId,
     category: String,
     subcategory: String,
     title: String,
@@ -49,39 +105,28 @@ struct Article {
 }
 
 #[derive(Debug, Deserialize)]
-struct ArticleProvider {
-    articles: HashMap<String, Article>,
+struct DocumentProvider {
+    articles: HashMap<DocumentId, Document>,
 }
 
-impl ArticleProvider {
+impl DocumentProvider {
     fn new(path: &str) -> Result<Self, anyhow::Error> {
-        let mut articles = HashMap::new();
-        for article in read::<Article>(path)? {
-            let article = article?;
-            articles.insert(article.id.clone(), article);
-        }
+        let articles = read::<Document>(path)?
+            .map(|article| article.map(|article| (article.id.clone(), article)))
+            .try_collect()?;
         Ok(Self { articles })
     }
 
-    fn sample(&self, n: usize) -> Vec<Article> {
-        let mut rng = thread_rng();
-        self.articles
-            .values()
-            .cloned()
-            .collect_vec()
-            .iter()
-            .choose_multiple(&mut rng, n)
-            .into_iter()
-            .cloned()
-            .collect()
+    fn sample(&self, n: usize) -> Vec<&Document> {
+        self.articles.values().choose_multiple(&mut thread_rng(), n)
     }
 
-    fn get(&self, id: &str) -> Option<Article> {
-        self.articles.get(id).cloned()
+    fn get(&self, id: &DocumentId) -> Option<&Document> {
+        self.articles.get(id)
     }
 }
 
-struct SnippetLabelPair(String, String);
+struct SnippetLabelPair(String, bool);
 
 fn read<T>(path: &str) -> Result<DeserializeRecordsIntoIter<File, T>, anyhow::Error>
 where
@@ -103,11 +148,11 @@ where
 
 /// Runs the user-based mind benchmark
 fn run_benchmark() -> Result<(), anyhow::Error> {
-    let article_provider = ArticleProvider::new("news.tsv")?;
+    let article_provider = DocumentProvider::new("news.tsv")?;
 
     let impressions = read("behaviors.tsv")?;
 
-    let nranks = vec![3];
+    let nranks = vec![2];
     let mut ndcgs = Array::zeros((nranks.len(), 0));
 
     // Loop over all impressions, prepare reranker with news in click history
@@ -116,8 +161,8 @@ fn run_benchmark() -> Result<(), anyhow::Error> {
         let impression: Impression = impression?;
 
         // Placeholder for interacting with the entire click history
-        for click in impression.clicks.split(' ') {
-            match article_provider.get(click) {
+        for click in impression.clicks {
+            match article_provider.get(&click) {
                 Some(article) => println!("The article {:?} was interacted.", article),
                 None => println!("Article id {} not found.", click),
             }
@@ -126,20 +171,20 @@ fn run_benchmark() -> Result<(), anyhow::Error> {
         // Placeholder for reranking the results
         let mut snippet_label_pairs = impression
             .news
-            .split(' ')
-            .filter_map(|x| {
-                x.split_once('-').and_then(|(id, label)| {
-                    article_provider
-                        .get(id)
-                        .map(|article| SnippetLabelPair(article.snippet, label.to_string()))
-                })
+            .iter()
+            .filter_map(|viewed_document| {
+                article_provider
+                    .get(&viewed_document.document_id)
+                    .map(|article| {
+                        SnippetLabelPair(article.snippet.clone(), viewed_document.was_clicked)
+                    })
             })
             .collect::<Vec<_>>();
         snippet_label_pairs.shuffle(&mut thread_rng());
 
         let labels = snippet_label_pairs
             .iter()
-            .map(|snippet_label| snippet_label.1.parse::<f32>().unwrap())
+            .map(|snippet_label| if snippet_label.1 { 1.0 } else { 0.0 })
             .collect::<Vec<_>>();
         let ndcgs_iteration = ndcg(&labels, &nranks);
 
