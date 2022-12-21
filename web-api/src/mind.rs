45fb4d8e24de12361306d3cd503edba82944ef02
Maciej Krajewski <maciejkrajewskiml@gmail.com>
2022-12-21 15:10:36 +0000

    ET-3590 setup persona related test workflow (#737)
    **References**
    
    [ET-3590](https://xainag.atlassian.net/browse/ET-3590?atlOrigin=eyJpIjoiNDExOGEyMjM0MzU0NDI0ZmEwYWQzY2E1YWI2OGEzZjciLCJwIjoiaiJ9)
    
    **Summary**
    This PR introduces persona-related workflow into `mind.rs` script. 
    
    
    
    Co-authored-by: Maciej Krajewski <81871099+krajewskiML@users.noreply.github.com>
    Co-authored-by: Jan Petsche <58227040+janpetschexain@users.noreply.github.com>

--- web-api/src/mind.rs
+++ web-api/src/mind.rs
@@ -16,15 +16,17 @@
 
 #![allow(dead_code)]
 
-use std::{collections::HashMap, fs::File, path::Path};
+use std::{collections::HashMap, fs::File, io, path::Path};
 
 use anyhow::Error;
 use csv::{DeserializeRecordsIntoIter, Reader, ReaderBuilder};
 use itertools::Itertools;
-use ndarray::{Array, ArrayView};
+use ndarray::{Array, Array3, ArrayView};
+use npyz::WriterBuilder;
 use rand::{
     seq::{IteratorRandom, SliceRandom},
     thread_rng,
+    Rng,
 };
 use serde::{de, Deserialize, Deserializer};
 use xayn_ai_coi::{nan_safe_f32_cmp_desc, CoiConfig, CoiSystem};
@@ -157,6 +159,28 @@ where
         .collect()
 }
 
+// struct that represents config of hyperparameters for the persona based benchmark
+#[derive(Debug, Deserialize)]
+struct PersonaBasedConfig {
+    click_probability: f32,
+    n_documents: usize,
+    iterations: usize,
+    amount_of_doc_used_to_prepare: usize,
+    nranks: Vec<usize>,
+}
+
+impl Default for PersonaBasedConfig {
+    fn default() -> Self {
+        Self {
+            click_probability: 0.5,
+            n_documents: 100,
+            iterations: 10,
+            amount_of_doc_used_to_prepare: 1,
+            nranks: vec![3, 5],
+        }
+    }
+}
+
 #[derive(Debug, Deserialize)]
 struct Impression {
     id: String,
@@ -176,6 +200,16 @@ struct Document {
     title: String,
     snippet: String,
     url: String,
+}
+
+impl Document {
+    // check if the document is interesting to the user
+    fn is_interesting(&self, user_interests: &[String]) -> bool {
+        user_interests.iter().any(|interest| {
+            let (main_category, sub_category) = interest.split_once('/').unwrap();
+            self.category == main_category || self.subcategory == sub_category
+        })
+    }
 }
 
 #[derive(Debug, Deserialize)]
@@ -200,6 +234,40 @@ impl DocumentProvider {
     fn get(&self, id: &DocumentId) -> Option<&Document> {
         self.documents.get(id)
     }
+
+    // get all documents that matches user's interest
+    fn get_all_interest(&self, interests: &[String]) -> Vec<&Document> {
+        self.documents
+            .values()
+            .filter(|doc| doc.is_interesting(interests))
+            .collect()
+    }
+}
+
+#[derive(Debug, derive_more::Deref, Deserialize)]
+struct Users(HashMap<UserId, Vec<String>>);
+
+impl Users {
+    // function that reads the users interests from a json file
+    fn new(path: &str) -> Result<Self, Error> {
+        let file = File::open(path)?;
+        let json = serde_json::from_reader::<_, serde_json::Value>(file)?;
+        let map = json.as_object().unwrap();
+        // iterate over map and create a map of user ids and their interests
+        Ok(Users(
+            map.iter()
+                .map(|(user_id, interests)| {
+                    let interests = interests
+                        .as_array()
+                        .unwrap()
+                        .iter()
+                        .map(|interest| interest.as_str().unwrap().to_string())
+                        .collect();
+                    (UserId::new(user_id).unwrap(), interests)
+                })
+                .collect(),
+        ))
+    }
 }
 
 struct SnippetLabelPair(String, bool);
@@ -222,8 +290,121 @@ where
         .map_err(Into::into)
 }
 
+// function that assigns a score to a vector of documents based on the user's interests
+// score is equal to 2 if the document is interesting to the user, 0 otherwise
+fn score_documents(documents: &[&Document], user_interests: &[String]) -> Vec<f32> {
+    documents
+        .iter()
+        .map(|document| {
+            if document.is_interesting(user_interests) {
+                2.0
+            } else {
+                0.0
+            }
+        })
+        .collect_vec()
+}
+
+fn write_array<T, S, D>(writer: impl io::Write, array: &ndarray::ArrayBase<S, D>) -> io::Result<()>
+where
+    T: Clone + npyz::AutoSerialize,
+    S: ndarray::Data<Elem = T>,
+    D: ndarray::Dimension,
+{
+    let shape = array.shape().iter().map(|&x| x as u64).collect_vec();
+    let c_order_items = array.iter();
+
+    let mut writer = npyz::WriteOptions::new()
+        .default_dtype()
+        .shape(&shape)
+        .writer(writer)
+        .begin_nd()?;
+    writer.extend(c_order_items)?;
+    writer.finish()
+}
+/// # Panics
+///
+/// The function panics if the provided filenames are not correct.
+#[tokio::test]
+#[ignore]
+async fn run_persona_benchmark() -> Result<(), Error> {
+    let document_provider = DocumentProvider::new("news.tsv")?;
+    let users_interests = Users::new("user_categories.json")?;
+    let state = State::new(Storage::default()).unwrap();
+    // load documents from document provider to state
+    state
+        .insert(document_provider.documents.values().cloned().collect_vec())
+        .await
+        .unwrap();
+    let benchmark_config = PersonaBasedConfig::default();
+    // create 3d array of zeros with shape (users, iterations, nranks)
+    let mut results = Array3::<f32>::zeros([
+        users_interests.len(),
+        benchmark_config.iterations,
+        benchmark_config.nranks.len(),
+    ]);
+
+    let mut rng = thread_rng();
+
+    for (idx, (user_id, interests)) in users_interests.iter().enumerate() {
+        let interesting_documents = document_provider.get_all_interest(interests);
+        let ids_of_documents_to_prepare = interesting_documents
+            .choose_multiple(&mut rng, benchmark_config.amount_of_doc_used_to_prepare)
+            .map(|doc| doc.id.clone())
+            .collect_vec();
+        // prepare reranker by interacting with documents to prepare
+        state
+            .interact(user_id, &ids_of_documents_to_prepare)
+            .await
+            .unwrap();
+
+        for iter in 0..benchmark_config.iterations {
+            let personalised_documents = state
+                .personalize(
+                    user_id,
+                    PersonalizeBy::KnnSearch(benchmark_config.n_documents),
+                )
+                .await
+                .unwrap();
+
+            let documents = personalised_documents
+                .iter()
+                .map(|id| document_provider.get(id).unwrap())
+                .collect_vec();
+
+            let scores = score_documents(&documents, interests);
+            let ndcgs_iteration = ndcg(&scores, &benchmark_config.nranks);
+            //save scores to results array
+            for (i, ndcg) in ndcgs_iteration.iter().enumerate() {
+                results[[idx, iter, i]] = *ndcg;
+            }
+            // interact with documents
+            state
+                .interact(
+                    user_id,
+                    &personalised_documents
+                        .iter()
+                        .zip(scores.iter())
+                        .filter(|(_, &score)| {
+                            (score - 2.0).abs() < 0.001
+                                || rng.gen_range(0.0..1.0) < benchmark_config.click_probability
+                        })
+                        .map(|(id, _)| id.clone())
+                        .collect_vec(),
+                )
+                .await
+                .unwrap();
+        }
+    }
+    let mut file = File::create("results/persona_based_benchmark_results.npy")?;
+    write_array(&mut file, &results)?;
+    Ok(())
+}
+
 /// Runs the user-based mind benchmark
-fn run_benchmark() -> Result<(), Error> {
+#[test]
+#[ignore]
+fn run_user_benchmark() -> Result<(), Error> {
     let document_provider = DocumentProvider::new("news.tsv")?;
 
     let impressions = read("behaviors.tsv")?;
@@ -255,13 +436,13 @@ fn run_benchmark() -> Result<(), Error> {
                         SnippetLabelPair(document.snippet.clone(), viewed_document.was_clicked)
                     })
             })
-            .collect::<Vec<_>>();
+            .collect_vec();
         snippet_label_pairs.shuffle(&mut thread_rng());
 
         let labels = snippet_label_pairs
             .iter()
             .map(|snippet_label| if snippet_label.1 { 1.0 } else { 0.0 })
-            .collect::<Vec<_>>();
+            .collect_vec();
         let ndcgs_iteration = ndcg(&labels, &nranks);
 
         ndcgs
@@ -298,10 +479,4 @@ fn ndcg(relevance: &[f32], k: &[usize]) -> Vec<f32> {
         .enumerate()
         .filter_map(|(i, ndcg)| k.contains(&(i + 1)).then_some(ndcg))
         .collect()
-}
-
-fn main() {
-    if let Err(e) = run_benchmark() {
-        eprintln!("{}", e);
-    }
 }
