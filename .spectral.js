1e61b49bd9aa180cc78fa2714a9b6b150cd84f2e
Andrea Corradi <andreac@unstable.it>
2022-12-21 11:54:13 +0000

    Openpapi linters (#741)
    Enable some of linters, these are the ones that do not break the API (too much).

--- .spectral.js
+++ .spectral.js
@@ -32,14 +32,11 @@ module.exports = {
       }
     },
     'major-version-in-path': 'off',
-    'string-boundary': 'off',
-    'array-boundary': 'off',
     'schema-description': 'off',
     'inline-property-schema': 'off',
     'content-entry-provided': 'off',
     'property-description': 'off',
     'oas3-api-servers': 'off',
-    'operation-id-naming-convention': 'off',
     'delete-body': 'off',
     'optional-request-body': 'off',
     'prohibit-summary-sentence-style': 'off',
@@ -47,8 +44,6 @@ module.exports = {
     'response-example-provided': 'off',
     'collection-array-property': 'off',
     'response-error-response-schema': 'off',
-    'enum-case-convention': 'off',
-    'operation-id-case-convention': 'off',
     'patch-request-content-type': 'off',
   }
 };
