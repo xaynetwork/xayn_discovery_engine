ec5a57075f79de9604b12b2b78bef84b83fd51b0
Philipp Korber <philipp@korber.dev>
2023-01-03 19:14:46 +0100

    panic if published_after is used with memory.rs

--- web-api/src/storage/memory.rs
+++ web-api/src/storage/memory.rs
@@ -285,6 +285,10 @@ impl storage::Document for Storage {
         &self,
         params: KnnSearchParams<'a>,
     ) -> Result<Vec<PersonalizedDocument>, Error> {
+        if params.published_after.is_some() {
+            unimplemented!(/*we don't need it for memory.rs*/);
+        }
+
         let excluded = params.excluded.iter().collect::<HashSet<_>>();
         let documents = self.documents.read().await;
         let documents = documents
