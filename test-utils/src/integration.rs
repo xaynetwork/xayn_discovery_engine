be2eecab312a0dc04407553de8ba651c051b5948
Philipp Korber <philipp@korber.dev>
2023-01-05 20:16:59 +0100

    wip

--- test-utils/src/integration.rs
+++ test-utils/src/integration.rs
@@ -13,17 +13,20 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use std::{
+    env,
     fs,
     path::PathBuf,
     process::{Command, Output, Stdio},
+    sync::atomic::{AtomicBool, Ordering},
     time::Duration,
 };
 
 use chrono::Local;
 use once_cell::sync::Lazy;
+use regex::{bytes::Regex as BytesRegex, Regex};
 use reqwest::Method;
 use scopeguard::{guard_on_success, OnSuccess, ScopeGuard};
-use sqlx::{postgres::PgConnectOptions, Connection, PgConnection};
+use sqlx::{Connection, PgConnection};
 use tokio::time::sleep;
 
 use crate::error::Panic;
@@ -161,16 +164,24 @@ async fn create_index(es_index_uri: &str) -> Result<(), Panic> {
 }
 
 async fn drop_index(es_index_uri: &str) -> Result<(), Panic> {
-    todo!(/*HTTP DELETE es_index_uri*/)
+    let response = reqwest::Client::new().delete(es_index_uri).send().await?;
+
+    if response.status().is_success() {
+        Ok(())
+    } else {
+        let body = response.text().await?;
+        panic!("Dropping index ({es_index_uri}) failed: {body}");
+    }
 }
 
 pub struct WebDevEnv<'a> {
-    id: &'a str,
-    pg_uri: &'a str,
-    es_uri: &'a str,
+    pub id: &'a str,
+    pub pg_uri: &'a str,
+    pub es_uri: &'a str,
 }
 
-pub async fn with_web_dev_env<T>(
+/// Runs given closure in a context where a run specific ES/PG index/db is created.
+pub async fn web_dev_integration_test_setup<T>(
     func: impl for<'a> FnOnce(WebDevEnv<'a>) -> Result<T, Panic>,
 ) -> Result<T, Panic> {
     just(&["web_dev_up"])?;
@@ -191,6 +202,62 @@ pub async fn with_web_dev_env<T>(
 
     Ok(res)
 }
+
+/// Remove all variables from this process environment (with some exceptions).
+///
+/// Exceptions:
+///
+/// - `PATH`
+/// - `LANG`
+/// - `PWD`
+///
+/// Additional exceptions to avoid potential complications
+/// with programs called by just, especially wrt to docker-compose
+/// or podamn-compose:
+///
+/// - `DBUS*`
+/// - `SYSTEMD*`
+/// - `USER*`
+/// - `DOCKER*`
+/// - `PODMAN*`
+/// - `XDG*`
+pub fn clear_env() {
+    // We need to make sure we only do it once as this can be called concurrently
+    // and `remove_var` is not reliably thread safe.
+    if ENV_CLEARED
+        .compare_exchange(false, true, Ordering::SeqCst, Ordering::SeqCst)
+        .is_ok()
+    {
+        for (key, _) in std::env::vars_os() {
+            let keep = key
+                .to_str()
+                .map(|key| ENV_PRUNE_EXCEPTIONS.is_match(key))
+                .unwrap_or_default();
+
+            if !keep {
+                std::env::remove_var(key)
+            }
+        }
+    }
+}
+
+static ENV_CLEARED: AtomicBool = AtomicBool::new(false);
+static ENV_PRUNE_EXCEPTIONS: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(
+        r#"^(?ix)
+        (?:PATH)
+        |(?:LANG)
+        |(?:PWD)
+        |(?:USER)
+        |(?:DBUS.*)
+        |(?:SYSTEMD.*)
+        |(?:DOCKER.*)
+        |(?:PODMAN.*)
+        |(?:XDG.*)
+        $"#,
+    )
+    .unwrap()
+});
 
 #[cfg(test)]
 mod tests {
