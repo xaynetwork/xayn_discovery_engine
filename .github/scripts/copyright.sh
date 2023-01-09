4b1940319aefbc5860b4a0fe126cd844e54dddb4
janpetschexain <58227040+janpetschexain@users.noreply.github.com>
2023-01-09 15:24:37 +0000

    update copyright check (#763)
    **Summary**
    
    - add 2023 to copyright check

--- .github/scripts/copyright.sh
+++ .github/scripts/copyright.sh
@@ -13,7 +13,7 @@ errors=0
 
 for file in $(files_to_check)
 do
-    if ! head -n 1 $file | grep 'Copyright 202[12] Xayn AG' > /dev/null
+    if ! head -n 1 $file | grep 'Copyright 202[123] Xayn AG' > /dev/null
     then
         echo "##[error] Missing copyright in $file"
         errors=$((errors + 1))
