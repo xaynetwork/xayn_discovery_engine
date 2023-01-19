712dbc186fc6f83dbbc00d4ba25ac69aa45085df
Philipp Korber <philipp@korber.dev>
2023-01-19 15:16:44 +0100

    Update web-api/elastic-search/create_es_index.sh
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>

--- web-api/elastic-search/create_es_index.sh
+++ web-api/elastic-search/create_es_index.sh
@@ -7,7 +7,7 @@ cd $(dirname $0)
 URL="${1:-http://localhost:9200/test_index}"
 
 # There is also `/_cluster/health` but using OPTION here works a bit better
-# even through the index doesn't yet exist.
+# even though the index doesn't yet exist.
 while ! curl -sf -X OPTIONS "${URL}" ; do
     echo "Waiting for elasticsearch";
     sleep 1;
