e02eaf26e0b75b439239cafda9610308ab969000
Philipp Korber <philipp@korber.dev>
2023-02-07 13:55:30 +0100

    test for semantic search min_similarity

--- integration-tests/tests/semantic_search.rs
+++ integration-tests/tests/semantic_search.rs
@@ -13,32 +13,57 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 use integration_tests::{send_assert, send_assert_json, test_two_apps, unchanged_config};
-use reqwest::StatusCode;
-use serde::Deserialize;
+use reqwest::{Client, StatusCode, Url};
+use serde::{Deserialize, Serialize};
 use serde_json::json;
+use xayn_ai_test_utils::error::Panic;
 use xayn_web_api::{Ingestion, Personalization};
 
+async fn ingest(
+    client: &Client,
+    base_url: &Url,
+    documents: &[IngestionDocument],
+) -> Result<(), Panic> {
+    send_assert(
+        client,
+        client
+            .post(base_url.join("/documents")?)
+            .json(&json!({ "documents": documents }))
+            .build()?,
+        StatusCode::CREATED,
+    )
+    .await;
+    Ok(())
+}
+
 #[tokio::test]
-async fn test_test_app() {
+async fn test_semantic_search() {
     test_two_apps::<Ingestion, Personalization, _>(
         unchanged_config,
         unchanged_config,
         |client, ingestion_url, personalization_url, _services| async move {
-            send_assert(
+            ingest(
                 &client,
-                client
-                    .post(ingestion_url.join("/documents")?)
-                    .json(&json!({
-                        "documents": [
-                            { "id": "d1", "snippet": "this is one sentence which we have" },
-                            { "id": "d2", "snippet": "duck duck quack", "properties": { "dodo": 4 } },
-                            { "id": "d3", "snippet": "this is another sentence which we have" },
-                        ]
-                    }))
-                    .build()?,
-                StatusCode::CREATED,
+                &ingestion_url,
+                &[
+                    IngestionDocument {
+                        id: "d1".into(),
+                        snippet: "this is one sentence which we have".into(),
+                        properties: None,
+                    },
+                    IngestionDocument {
+                        id: "d2".into(),
+                        snippet: "duck duck quack".into(),
+                        properties: Some(json!({ "dodo": 4 })),
+                    },
+                    IngestionDocument {
+                        id: "d3".into(),
+                        snippet: "this is another sentence which we have".into(),
+                        properties: None,
+                    },
+                ],
             )
-            .await;
+            .await?;
 
             let SearchResults { documents } = send_assert_json(
                 &client,
@@ -65,15 +90,78 @@ async fn test_test_app() {
     .await;
 }
 
+#[tokio::test]
+async fn test_semantic_search_min_similarity() {
+    test_two_apps::<Ingestion, Personalization, _>(
+        unchanged_config,
+        unchanged_config,
+        |client, ingestion_url, personalization_url, _services| async move {
+            ingest(
+                &client,
+                &ingestion_url,
+                &[
+                    IngestionDocument {
+                        id: "d1".into(),
+                        snippet: "this is one sentence which we have".into(),
+                        properties: None,
+                    },
+                    IngestionDocument {
+                        id: "d2".into(),
+                        snippet: "1".into(),
+                        properties: Some(json!({ "dodo": 4 })),
+                    },
+                    IngestionDocument {
+                        id: "d3".into(),
+                        snippet: "this is another sentence which we have".into(),
+                        properties: None,
+                    },
+                ],
+            )
+            .await?;
+
+            let SearchResults { documents } = send_assert_json(
+                &client,
+                client
+                    .get(personalization_url.join("/semantic_search/d1")?)
+                    .query(&[("min_similarity", "0.5")])
+                    .build()?,
+                StatusCode::OK,
+            )
+            .await;
+
+            if let [first, second] = &documents[..] {
+                assert_eq!(first.id, "d3");
+                assert_eq!(second.id, "d2");
+                assert!(first.score > second.score);
+                assert!(first.properties.is_null());
+                assert_eq!(second.properties, json!({ "dodo": 4 }))
+            } else {
+                panic!("Unexpected number of documents: {:?}", documents);
+            }
+
+            Ok(())
+        },
+    )
+    .await;
+}
+
 #[derive(Deserialize)]
 struct SearchResults {
-    documents: Vec<Document>,
+    documents: Vec<PersonalizationDocument>,
 }
 
 #[derive(Debug, Deserialize)]
-struct Document {
+struct PersonalizationDocument {
     id: String,
     score: f32,
     #[serde(default)]
     properties: serde_json::Value,
+}
+
+#[derive(Serialize)]
+struct IngestionDocument {
+    id: String,
+    snippet: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    properties: Option<serde_json::Value>,
 }
