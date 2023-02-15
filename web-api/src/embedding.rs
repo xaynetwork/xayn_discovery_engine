44606187a760ccaebd49eb3649e7a92f11cd0cdb
vadim.popov <vadim.popov@xayn.com>
2023-02-15 11:31:16 +0100

    Sqashed all previous commits.
    testing the api in the ci
    
    fixed jobs tag
    
    fixed on tag
    
    fixed on tag
    
    fixed python version
    
    fixed python version
    
    fixed python version
    
    fixed python version
    
    fixed python version
    
    trying on debian
    
    trying on debian
    
    trying on ubuntu
    
    fixing command
    
    fixing python version
    
    fixing python version
    
    fixing python version
    
    fixing python version
    
    trying different runner
    
    fixing python version
    
    fixing python version
    
    fixing ubuntu version
    
    fixing pip command
    
    fixing pip command
    
    adding pip insallation
    
    debugging pip
    
    debugging pip
    
    debugging pip
    
    debugging pip
    
    debugging pip
    
    debugging pip
    
    added python to dockerfile
    
    switched back to hetzner
    
    update release tag for the ci-image.yml
    
    debugging
    
    debugging
    
    added python to path
    
    debugging
    
    return back PATH
    
    fixing yaml
    
    adding build commands
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging

--- web-api/src/embedding.rs
+++ web-api/src/embedding.rs
@@ -57,8 +57,9 @@ impl Embedder {
         let path = config.directory.relative();
         if !path.exists() {
             bail!(
-                "Fail to load Embedder: asset dir missing: {}",
-                path.display()
+              "Fail to load Embedder: asset dir missing :: {}; pwd: {}",
+               path.display(),
+               std::env::current_dir().unwrap().display(),
             );
         }
         let config_file = path.join("config.toml");
