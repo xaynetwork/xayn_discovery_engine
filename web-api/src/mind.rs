f11e21c1391c7ffdb426f14dbf93c78570fb9964
maciejkrajewski <maciejkrajewskiml@gmail.com>
2023-02-17 12:22:15 +0100

    review changes

--- web-api/src/mind.rs
+++ web-api/src/mind.rs
@@ -26,8 +26,9 @@ use rand::{
     seq::{IteratorRandom, SliceRandom},
     Rng,
     SeedableRng,
+    rngs::StdRng,
 };
-use serde::{de, Deserialize, Deserializer};
+use serde::{de, Deserialize, Deserializer, Serialize};
 use xayn_ai_coi::{nan_safe_f32_cmp_desc, CoiConfig, CoiSystem};
 
 use crate::{
@@ -75,7 +76,7 @@ impl State {
         })
     }
 
-    fn replace_coi_config(&mut self, config: CoiConfig) {
+    fn with_coi_config(&mut self, config: CoiConfig) {
         self.coi = config.build();
     }
 
@@ -195,10 +196,10 @@ struct GridSearchConfig {
     shifts: Vec<f32>,
     min_pos_cois: Vec<usize>,
     click_probability: f64,
-    n_documents: usize,
+    ndocuments: usize,
     iterations: usize,
     nranks: Vec<usize>,
-    semi_interesting_flag: bool,
+    is_semi_interesting: bool,
 }
 
 impl Default for GridSearchConfig {
@@ -208,10 +209,10 @@ impl Default for GridSearchConfig {
             shifts: vec![0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4],
             min_pos_cois: vec![1],
             click_probability: 0.2,
-            n_documents: 100,
+            ndocuments: 100,
             iterations: 10,
             nranks: vec![3, 5],
-            semi_interesting_flag: false,
+            is_semi_interesting: false,
         }
     }
 }
@@ -220,24 +221,24 @@ impl Default for GridSearchConfig {
 #[derive(Debug, Deserialize)]
 struct PersonaBasedConfig {
     click_probability: f64,
-    n_documents: usize,
+    ndocuments: usize,
     iterations: usize,
     amount_of_doc_used_to_prepare: usize,
     nranks: Vec<usize>,
-    n_documents_hot_news: usize,
-    semi_interesting_flag: bool,
+    ndocuments_hot_news: usize,
+    is_semi_interesting: bool,
 }
 
 impl Default for PersonaBasedConfig {
     fn default() -> Self {
         Self {
             click_probability: 0.2,
-            n_documents: 100,
+            ndocuments: 100,
             iterations: 10,
             amount_of_doc_used_to_prepare: 1,
             nranks: vec![3, 5],
-            n_documents_hot_news: 15,
-            semi_interesting_flag: false,
+            ndocuments_hot_news: 15,
+            is_semi_interesting: false,
         }
     }
 }
@@ -245,7 +246,7 @@ impl Default for PersonaBasedConfig {
 #[derive(Debug, Deserialize)]
 struct SaturationConfig {
     click_probability: f64,
-    n_documents: usize,
+    ndocuments: usize,
     iterations: usize,
 }
 
@@ -253,13 +254,13 @@ impl Default for SaturationConfig {
     fn default() -> Self {
         Self {
             click_probability: 0.2,
-            n_documents: 30,
+            ndocuments: 30,
             iterations: 10,
         }
     }
 }
 
-#[derive(Clone, Debug, serde::Serialize)]
+#[derive(Clone, Debug, Serialize)]
 struct StateConfig {
     coi: CoiConfig,
     personalization: PersonalizationConfig,
@@ -276,38 +277,38 @@ impl Default for StateConfig {
     }
 }
 
-// structure that represents the results of iteration of the saturation benchmark
-#[derive(Debug, Default, serde::Serialize)]
-struct SaturationIterationResult {
-    shown_documents: Vec<DocumentId>,
+// The results of iteration of the saturation benchmark
+#[derive(Debug, Default, Serialize)]
+struct SaturationIteration {
+    showndocuments: Vec<DocumentId>,
     clicked_documents: Vec<DocumentId>,
 }
 
 // structure that represents the results of the saturation benchmark for one topic
-#[derive(Debug, Default, serde::Serialize)]
+#[derive(Debug, Default, Serialize)]
 struct SaturationTopicResult {
     topic: String,
-    iterations: Vec<SaturationIterationResult>,
+    iterations: Vec<SaturationIteration>,
 }
 
 impl SaturationTopicResult {
-    fn new(topic: &str) -> Self {
+    fn new(topic: &str, iterations: usize) -> Self {
         Self {
             topic: topic.to_owned(),
-            iterations: vec![],
+            iterations: Vec::with_capacity(iterations),
         }
     }
 }
 
 // structure that represents the results of the saturation benchmark
-#[derive(Debug, Default, serde::Serialize)]
+#[derive(Debug, Default, Serialize)]
 struct SaturationResult {
     topics: Vec<SaturationTopicResult>,
 }
 
 impl SaturationResult {
-    fn new() -> Self {
-        Self { topics: vec![] }
+    fn new(topics: usize) -> Self {
+        Self { topics: Vec::with_capacity(topics) }
     }
 }
 
@@ -370,7 +371,7 @@ impl DocumentProvider {
     fn sample(&self, n: usize) -> Vec<&Document> {
         self.documents
             .values()
-            .choose_multiple(&mut rand::rngs::StdRng::seed_from_u64(42), n)
+            .choose_multiple(&mut StdRng::seed_from_u64(42), n)
     }
 
     fn get(&self, id: &DocumentId) -> Option<&Document> {
@@ -395,14 +396,8 @@ struct SpecificTopics(Vec<String>);
 impl SpecificTopics {
     fn new(path: &str) -> Result<Self, Error> {
         let file = File::open(path)?;
-        let json = serde_json::from_reader::<_, serde_json::Value>(file)?;
-        let list = json.as_array().unwrap();
-        // iterate over vector and create a pair of strings
-        Ok(SpecificTopics(
-            list.iter()
-                .map(|interest| interest.as_str().unwrap().to_string())
-                .collect(),
-        ))
+        let topics = serde_json::from_reader::<_, Vec<String>>(file)?;
+        Ok(Self(topics))
     }
 }
 
@@ -458,19 +453,15 @@ where
 fn score_documents(
     documents: &[&Document],
     user_interests: &[String],
-    semi_interesting_flag: bool,
+    is_semi_interesting: bool,
 ) -> Vec<f32> {
     documents
         .iter()
         .map(|document| {
             if document.is_interesting(user_interests) {
                 2.0
-            } else if semi_interesting_flag {
-                if document.is_semi_interesting(user_interests) {
-                    1.0
-                } else {
-                    0.0
-                }
+            } else if is_semi_interesting &&  document.is_semi_interesting(user_interests) {
+                1.0
             } else {
                 0.0
             }
@@ -517,7 +508,7 @@ async fn run_persona_benchmark() -> Result<(), Error> {
         benchmark_config.nranks.len(),
     ]);
 
-    let mut rng = rand::rngs::StdRng::seed_from_u64(42);
+    let mut rng = StdRng::seed_from_u64(42);
     for (idx, (user_id, interests)) in users_interests.iter().enumerate() {
         let interesting_documents = document_provider.get_all_interest(interests);
         let ids_of_documents_to_prepare = interesting_documents
@@ -541,7 +532,7 @@ async fn run_persona_benchmark() -> Result<(), Error> {
                 .personalize(
                     user_id,
                     PersonalizeBy::KnnSearch {
-                        count: benchmark_config.n_documents,
+                        count: benchmark_config.ndocuments,
                         published_after: None,
                     },
                     state.time,
@@ -558,7 +549,7 @@ async fn run_persona_benchmark() -> Result<(), Error> {
             let scores = score_documents(
                 &documents,
                 interests,
-                benchmark_config.semi_interesting_flag,
+                benchmark_config.is_semi_interesting,
             );
             let ndcgs_iteration = ndcg(&scores, &benchmark_config.nranks);
             //save scores to results array
@@ -679,12 +670,12 @@ async fn run_user_benchmark() -> Result<(), Error> {
 /// The function panics if the provided filenames are not correct.
 #[tokio::test]
 #[ignore]
-async fn run_saturation_test() -> Result<(), Error> {
+async fn run_saturation_benchmark() -> Result<(), Error> {
     // load list of possible specific topics from file (need to create it)
     let specific_topics = SpecificTopics::new(
-        "/Users/maciejkrajewski/CLionProjects/xayn_discovery_engine/web-api/src/bin/topics.json",
+        "topics.json",
     )?;
-    let document_provider = DocumentProvider::new("/Users/maciejkrajewski/CLionProjects/xayn_discovery_engine/web-api/src/bin/news_cleaned.tsv")?;
+    let document_provider = DocumentProvider::new("news.tsv")?;
 
     let state = State::new(Storage::default(), StateConfig::default()).unwrap();
     // load documents from document provider to state
@@ -695,18 +686,18 @@ async fn run_saturation_test() -> Result<(), Error> {
     let benchmark_config = SaturationConfig::default();
 
     // create rng thread for random number generation with seed 42
-    let mut rng = rand::rngs::StdRng::seed_from_u64(42);
+    let mut rng = StdRng::seed_from_u64(42);
 
     // create results preserving structure
-    let mut results = SaturationResult::new();
+    let mut results = SaturationResult::new(specific_topics.len());
 
     // iterate over specific topics get the documents for each topic and interact with one document
     // from each topic and then run iterations of personalized search
     for full_category in specific_topics.iter() {
         //create saturation topic result structure
-        let mut topic_result = SaturationTopicResult::new(full_category);
+        let mut topic_result = SaturationTopicResult::new(full_category, benchmark_config.iterations);
 
-        let documents = document_provider.get_all_interest(&[full_category.clone()]);
+        let documents = document_provider.get_all_interest(std::slice::from_ref(full_category));
         let user_id = UserId::new(full_category).unwrap();
         // get random document from the topic
         let document = documents.choose(&mut rng).unwrap();
@@ -725,7 +716,7 @@ async fn run_saturation_test() -> Result<(), Error> {
                 .personalize(
                     &user_id,
                     PersonalizeBy::KnnSearch {
-                        count: benchmark_config.n_documents,
+                        count: benchmark_config.ndocuments,
                         published_after: None,
                     },
                     Utc::now(),
@@ -742,11 +733,12 @@ async fn run_saturation_test() -> Result<(), Error> {
             let scores = score_documents(&documents, &[full_category.clone()], false);
             let to_be_clicked = personalised_documents
                 .iter()
-                .zip(scores.iter())
-                .filter(|(_, &score)| {
-                    (score - 2.0).abs() < 0.001 && rng.gen_bool(benchmark_config.click_probability)
+                .zip(scores)
+                .filter_map(|(id, score)| {
+                    ((score - 2.0).abs() < 0.001
+                        && rng.gen_bool(benchmark_config.click_probability))
+                        .then(|| id.clone())
                 })
-                .map(|(id, _)| id.clone())
                 .collect_vec();
             // interact with documents
             state
@@ -761,8 +753,8 @@ async fn run_saturation_test() -> Result<(), Error> {
                 .unwrap();
 
             // add results to the topic result
-            topic_result.iterations.push(SaturationIterationResult {
-                shown_documents: personalised_documents,
+            topic_result.iterations.push(SaturationIteration {
+                showndocuments: personalised_documents,
                 clicked_documents: to_be_clicked,
             });
         }
@@ -795,7 +787,7 @@ async fn run_persona_hot_news_benchmark() -> Result<(), Error> {
         benchmark_config.nranks.len(),
     ]);
 
-    let mut rng = rand::rngs::StdRng::seed_from_u64(42);
+    let mut rng = StdRng::seed_from_u64(42);
 
     for (idx, (user_id, interests)) in users_interests.iter().enumerate() {
         let interesting_documents = document_provider.get_all_interest(interests);
@@ -811,7 +803,7 @@ async fn run_persona_hot_news_benchmark() -> Result<(), Error> {
 
         for iter in 0..benchmark_config.iterations {
             // get random news that will represent tenant's hot news category
-            let hot_news = document_provider.sample(benchmark_config.n_documents_hot_news); // number of news will be configurable
+            let hot_news = document_provider.sample(benchmark_config.ndocuments_hot_news); // number of news will be configurable
 
             // go through hot news and if they are in user's sphere of interests then click them with some probability
             state
@@ -819,11 +811,11 @@ async fn run_persona_hot_news_benchmark() -> Result<(), Error> {
                     user_id,
                     &hot_news
                         .iter()
-                        .filter(|doc| {
-                            doc.is_interesting(interests)
-                                && rng.gen_bool(benchmark_config.click_probability)
+                        .filter_map(|doc| {
+                            (doc.is_interesting(interests)
+                                && rng.gen_bool(benchmark_config.click_probability))
+                                .then(|| (doc.id.clone(), state.time - Duration::days(0)))
                         })
-                        .map(|doc| (doc.id.clone(), state.time - Duration::days(0)))
                         .collect_vec(),
                 )
                 .await
@@ -833,7 +825,7 @@ async fn run_persona_hot_news_benchmark() -> Result<(), Error> {
                 .personalize(
                     user_id,
                     PersonalizeBy::KnnSearch {
-                        count: benchmark_config.n_documents,
+                        count: benchmark_config.ndocuments,
                         published_after: None,
                     },
                     Utc::now(),
@@ -850,25 +842,25 @@ async fn run_persona_hot_news_benchmark() -> Result<(), Error> {
             let scores = score_documents(
                 &documents,
                 interests,
-                benchmark_config.semi_interesting_flag,
+                benchmark_config.is_semi_interesting,
             );
             let ndcgs_iteration = ndcg(&scores, &benchmark_config.nranks);
             //save scores to results array
-            for (i, ndcg) in ndcgs_iteration.iter().enumerate() {
-                results[[idx, iter, i]] = *ndcg;
-            }
+            results
+                .slice_mut(ndarray::s![idx, iter, ..])
+                .assign(&Array::from(ndcgs_iteration));
             // interact with documents
             state
                 .interact(
                     user_id,
                     &personalised_documents
                         .iter()
-                        .zip(scores.iter())
-                        .filter(|(_, &score)| {
-                            (score - 2.0).abs() < 0.001
-                                && rng.gen_bool(benchmark_config.click_probability)
+                        .zip(scores)
+                        .filter_map(|(id, score)| {
+                            ((score - 2.0).abs() < 0.001
+                                && rng.gen_bool(benchmark_config.click_probability))
+                                .then(|| (id.clone(), state.time - Duration::days(0)))
                         })
-                        .map(|(id, _)| (id.clone(), state.time - Duration::days(0)))
                         .collect_vec(),
                 )
                 .await
@@ -888,8 +880,7 @@ async fn grid_search_for_best_parameters() -> Result<(), Error> {
     let users_interests = Users::new("user_categories_sample.json")?;
     let document_provider = DocumentProvider::new("news.tsv")?;
     let grid_search_config = GridSearchConfig::default();
-
-    let mut configs = Vec::new();
+    let mut configs = Vec::with_capacity(grid_search_config.thresholds.len() * grid_search_config.shifts.len() * grid_search_config.min_pos_cois.len(), );
 
     let start_time = Utc::now();
 
@@ -917,7 +908,7 @@ async fn grid_search_for_best_parameters() -> Result<(), Error> {
         .insert(document_provider.to_documents())
         .await
         .unwrap();
-    let mut rng = rand::rngs::StdRng::seed_from_u64(42);
+    let mut rng = StdRng::seed_from_u64(42);
     let file = File::create("params.json")?;
     let mut writer = io::BufWriter::new(file);
     serde_json::to_writer(&mut writer, &configs)?;
@@ -929,9 +920,9 @@ async fn grid_search_for_best_parameters() -> Result<(), Error> {
         grid_search_config.iterations,
         grid_search_config.nranks.len(),
     ));
-    for (config_idx, config) in configs.iter().enumerate() {
+    for (config_idx, config) in configs.into_iter().enumerate() {
         println!("{config_idx:?}");
-        state.replace_coi_config(config.coi.clone());
+        state.with_coi_config(config.coi);
         // go through users
         for (idx, (user_id, interests)) in users_interests.iter().enumerate() {
             let interesting_documents = document_provider.get_all_interest(interests);
@@ -950,7 +941,7 @@ async fn grid_search_for_best_parameters() -> Result<(), Error> {
                     .personalize(
                         user_id,
                         PersonalizeBy::KnnSearch {
-                            count: grid_search_config.n_documents,
+                            count: grid_search_config.ndocuments,
                             published_after: None,
                         },
                         Utc::now(),
@@ -967,25 +958,25 @@ async fn grid_search_for_best_parameters() -> Result<(), Error> {
                 let scores = score_documents(
                     &documents,
                     interests,
-                    grid_search_config.semi_interesting_flag,
+                    grid_search_config.is_semi_interesting,
                 );
                 let ndcgs_iteration = ndcg(&scores, &grid_search_config.nranks);
                 //save scores to results array
-                for (i, ndcg) in ndcgs_iteration.iter().enumerate() {
-                    ndcgs_all_configs[[config_idx, idx, iter, i]] = *ndcg;
-                }
+                ndcgs_all_configs
+                    .slice_mut(ndarray::s![config_idx, idx, iter,..])
+                    .assign(&Array::from(ndcgs_iteration));
                 // interact with documents
                 state
                     .interact(
                         user_id,
                         &personalised_documents
                             .iter()
-                            .zip(scores.iter())
-                            .filter(|(_, &score)| {
-                                (score - 2.0).abs() < 0.001
-                                    && rng.gen_bool(grid_search_config.click_probability)
+                            .zip(scores)
+                            .filter_map(|(id, score)| {
+                                ((score - 2.0).abs() < 0.001
+                                    && rng.gen_bool(grid_search_config.click_probability))
+                                    .then(|| (id.clone(), state.time))
                             })
-                            .map(|(id, _)| (id.clone(), state.time - Duration::days(0)))
                             .collect_vec(),
                     )
                     .await
