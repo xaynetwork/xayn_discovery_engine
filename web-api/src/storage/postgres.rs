826364cfdb68f3321ead515dd04b791696c9ccb5
janpetschexain <58227040+janpetschexain@users.noreply.github.com>
2023-03-22 09:39:37 +0000

    ET-3837 fix changed pg migration file 2 (#857)
    **Reference**
    
    - [ET-3837]
    - #856
    
    **Summary**
    
    - fix internal migrations history table: the checksum only changed because of the fixed comment
    
    
    [ET-3837]: https://xainag.atlassian.net/browse/ET-3837?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- web-api/src/storage/postgres.rs
+++ web-api/src/storage/postgres.rs
@@ -37,8 +37,8 @@ use sqlx::{
     Transaction,
 };
 #[cfg(feature = "ET-3837")]
-use tracing::{debug, error};
-use tracing::{info, instrument, warn};
+use tracing::debug;
+use tracing::{error, info, instrument, warn};
 use xayn_ai_bert::NormalizedEmbedding;
 use xayn_ai_coi::{CoiId, CoiStats, NegativeCoi, PositiveCoi, UserInterests};
 
@@ -136,16 +136,26 @@ impl Config {
         let options = self.build_connection_options()?;
         info!("starting postgres setup");
         let pool = PoolOptions::new().connect_with(options).await?;
+
+        match sqlx::query::<Postgres>(
+            "UPDATE _sqlx_migrations
+            SET checksum = decode('80482d7c5870ce78cbd73c39d498f6375fe260fde7b594085200aa2058c886a19e2de188a7799b202d0fe2c040460c97', 'hex')
+            WHERE version = 20230216164200;"
+        ).execute(&pool).await {
+            Ok(result) => {
+                let updated = result.rows_affected();
+                if updated == 1 {
+                    info!("updated migrations history");
+                } else {
+                    error!(updated, "failed to update migrations history");
+                }
+            }
+            Err(sqlx::Error::Database(error)) => info!(?error, "migrations history doesn't exist"),
+            Err(error) => return Err(error),
+        }
+
         if !self.skip_migrations {
-            let mut migrator = sqlx::migrate!();
-            migrator.run(&pool).await?;
-            migrator.undo(&pool, 20_221_214_154_900).await?;
-            migrator
-                .migrations
-                .to_mut()
-                .last_mut()
-                .replace(&mut sqlx::migrate!("./migrations_ET_3837").migrations.to_mut()[0]);
-            migrator.run(&pool).await?;
+            sqlx::migrate!().run(&pool).await?;
         }
 
         Ok(Database { pool })
