4e4ffe1ed3c49397ba62465229faaed7ac4d27db
Andrea Corradi <andrea.corradi@xayn.com>
2023-03-30 13:20:13 +0200

    Revert not needed changes

--- .github/scripts/download_assets.sh
+++ .github/scripts/download_assets.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 set -eu -o pipefail
 
-DATA_DIR="$PWD/../../assets"
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+# path to the directory where this file is
+SELF_DIR_PATH="$(dirname "$0")"
+
+# a parameter for the destination of the assets can be passed.
+# the default is the directory assets, we assume the script is in .github/scripts/
+DATA_DIR="${1:-$SELF_DIR_PATH/../../assets}"
+DATA_DIR=`realpath $DATA_DIR`
+
 CHECKSUM_FILE="sha256sums"
-BASE_URL="http://s3-de-central.profitbricks.com/xayn-yellow-bert"
 
 download()
 {
-    ARCHIVE_BASENAME="$1_$2"
-    ARCHIVE_NAME="$ARCHIVE_BASENAME.tgz"
-    TMP_ARCHIVE_NAME="$ARCHIVE_NAME.tmp"
+  NAME="$1"
+  VERSION="$2"
+  ARCHIVE_BASENAME="${NAME}_$VERSION"
+  ARCHIVE_NAME="$ARCHIVE_BASENAME.tgz"
+  TMP_ARCHIVE_NAME="$ARCHIVE_NAME.tmp"
+  URL="http://s3-de-central.profitbricks.com/xayn-yellow-bert/$NAME/$ARCHIVE_NAME"
 
-    if [ -f "$DATA_DIR/$ARCHIVE_NAME" ]; then
-        echo "skip downloading $DATA_DIR/$ARCHIVE_NAME"
-    else
-        curl "$BASE_URL/$1/$ARCHIVE_NAME" -o "$DATA_DIR/$TMP_ARCHIVE_NAME" -C -
-        mv "$DATA_DIR/$TMP_ARCHIVE_NAME" "$DATA_DIR/$ARCHIVE_NAME"
+  if [  -f "$DATA_DIR/$ARCHIVE_NAME" ]; then
+    echo "skip downloading $DATA_DIR/$ARCHIVE_NAME"
+  else
 
-        cd "$DATA_DIR"
-        tar -zxf "$ARCHIVE_NAME"
+    curl "$URL" -o "$DATA_DIR/$TMP_ARCHIVE_NAME" -C -
+    mv "$DATA_DIR/$TMP_ARCHIVE_NAME" "$DATA_DIR/$ARCHIVE_NAME"
 
-        # check content
-        cd "$ARCHIVE_BASENAME"
-        shasum -c "$CHECKSUM_FILE"
-    fi
+    cd "$DATA_DIR"
+    tar -zxf "$ARCHIVE_NAME"
+
+    # check content
+    cd "$ARCHIVE_BASENAME"
+    shasum -c "$CHECKSUM_FILE"
+  fi
 }
 
 if [ $# -gt 0 ]; then
