8bc1433ba2a181ddf02f70c0b4a91ad917fb2be2
Andrea Corradi <andrea.corradi@xayn.com>
2023-04-26 12:54:50 +0200

    Fix doc

--- summarizer/src/lib.rs
+++ summarizer/src/lib.rs
@@ -12,6 +12,8 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+//! Summarize a large text. It uses a probabilistic approach.
+
 #![forbid(unsafe_op_in_unsafe_fn)]
 #![forbid(unsafe_code)]
 #![deny(
