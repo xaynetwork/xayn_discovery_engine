b7ac01024dd225c8162b65bfaa4fb81673599842
Andrea Corradi <andrea.corradi@xayn.com>
2023-06-06 17:59:38 +0200

    Fail

--- web-api-db-ctrl/postgres/tenant/20220926145400_document.sql
+++ web-api-db-ctrl/postgres/tenant/20220926145400_document.sql
@@ -12,6 +12,7 @@
 --  You should have received a copy of the GNU Affero General Public License
 --  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
 CREATE TABLE IF NOT EXISTS interaction (
     doc_id TEXT NOT NULL,
     user_id TEXT NOT NULL,
