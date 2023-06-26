5cc17428c9d2b03180aab3f16335f8af468a499f
Andrea Corradi <andrea.corradi@xayn.com>
2023-06-26 15:17:32 +0200

    Empty strings are valid properties and other changes (#1000)
    * Empty strings are valid properties
    
    * Fix function, add tests
    
    * Fix style
    
    * Keep null values
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Change spec
    
    * Fix version
    
    * chenge version
    
    * Update changelog
    
    * Update .spectral.js
    
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>
    
    * Update web-api/openapi/schemas/document.yml
    
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>

--- .spectral.js
+++ .spectral.js
@@ -50,6 +50,8 @@ module.exports = {
     },
     'ibm-major-version-in-path': 'off',
     'ibm-schema-description': 'off',
+    // spectral has its own rules to check schemas that support v3.1
+    'ibm-schema-type-format': 'off',
     'ibm-avoid-inline-schemas': 'off',
     'ibm-property-description': 'off',
     'oas3-api-servers': 'off',
