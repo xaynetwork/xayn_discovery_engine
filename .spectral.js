9b54bb95ff2e7ceca69fdac525719428ba50a374
Andrea Corradi <andrea.corradi@xayn.com>
2023-06-26 15:03:13 +0200

    Update .spectral.js
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>

--- .spectral.js
+++ .spectral.js
@@ -50,7 +50,7 @@ module.exports = {
     },
     'ibm-major-version-in-path': 'off',
     'ibm-schema-description': 'off',
-    // spectral has is own rules to check schema that support v3.1
+    // spectral has its own rules to check schemas that support v3.1
     'ibm-schema-type-format': 'off',
     'ibm-avoid-inline-schemas': 'off',
     'ibm-property-description': 'off',
