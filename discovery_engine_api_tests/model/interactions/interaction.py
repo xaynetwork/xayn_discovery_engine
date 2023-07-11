b2aef733064fe6bd9c4e878c2056cb46ef626bc8
joecks <joecks@users.noreply.github.com>
2023-07-11 14:29:54 +0000

    deploy: 83a567541a53b851adec826210871f174cd75a11

--- discovery_engine_api_tests/model/interactions/interaction.py
+++
@@ -1,6 +0,0 @@
-from model.base.model_base import ModelBase
-
-
-class Interaction(ModelBase):
-    def __init__(self, id):
-        self.id = id
--- discovery_engine_api_tests/model/interactions/interactions.py
+++
@@ -1,9 +0,0 @@
-from model.base.model_base import ModelBase
-
-
-class Interactions(ModelBase):
-    def __init__(self, *docs):
-        self.documents = []
-        for doc in docs:
-            self.documents.append(doc)
-
