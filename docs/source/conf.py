4da0d344c003cf24764ee0a719aec6bd966240fc
joecks <simon.joecks@gmail.com>
2023-07-18 14:30:18 +0200

    ET-3610 better documentation (#1021)
    * Start documentation page
    
    * Delete openapi dir
    
    * adjust readthedocs config
    
    * Add dark logo
    
    * Build all the documentation on github.io
    
    * Final documentation
    
    * Adjust colors and fonts of redocly to match those of the faro theme
    
    * Add CNAME during build
    
    * Fix some of the links
    
    * Update docs/source/index.rst
    
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>
    
    * phrasing
    
    * Changes from code review
    
    * Update gen_doc.yml
    
    ---------
    
    Co-authored-by: janpetschexain <58227040+janpetschexain@users.noreply.github.com>

---
+++ docs/source/conf.py
@@ -0,0 +1,66 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Documentation'
+copyright = '2023, Xayn'
+author = 'Xayn'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinxcontrib.httpdomain', 'sphinxcontrib.openapi',
+              'sphinx_toolbox.sidebar_links', 'myst_parser', 'sphinx_toolbox.github']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+
+html_theme_options = {
+    "light_logo": "logo.svg",
+    "dark_logo": "logo_dark.svg",
+    "sidebar_hide_name": True,
+    "top_of_page_button": "edit",
+    "light_css_variables": {
+        "color-brand-primary": "#544998",
+        "color-brand-content": "#544998",
+        "color-foreground-primary": "#1B2524",
+        "color-api-name": "#544998",
+        "color-highlight-on-target": "#f8f9fb",
+    },
+    "dark_css_variables": {
+        "color-brand-primary": "#b3adcf",
+        "color-brand-content": "#b3adcf",
+        # "color-foreground-primary": "#1B2524",
+        "color-api-name": "#8d87b7",
+        "color-highlight-on-target": "#c7cfdfaa",
+    },
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/xaynetwork/xayn_discovery_engine",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+}
+
+html_static_path = ['_static']
+# html_logo = "logo.svg"
+pygments_style = "bw"
+# pygments_dark_style = "bw"
+openapi_default_renderer = 'httpdomain'
+github_repository = "xayn_discovery_engine"
+github_username = "xaynetwork"
---
+++ docs/source/index.rst
@@ -0,0 +1,33 @@
+Xayn Api 
+========
+
+The API allows you to integrate Xayn's Personalised Semantic Search and Recommendations service into your systems. 
+
+The API is divided into two parts: the back office and the front office. The back office allows you to manage everything related to your content. In the Xayn service, every piece of content is managed as _document_'s. 
+The front office allows you to interact with the service to manage the users' interactions (clicks, reading, viewing) with your content, request recommendations, or perform searches.
+
+|Overview|
+
+.. centered::  A demo app that showcases, some of our features (Movies_Demo_)
+
+.. |Overview| image:: _static/overview.jpg
+  :width: 800
+  :alt: Visualization of an Example Application that uses some parts of the Xayn API
+
+.. _Movies_Demo: https://xaynetwork.github.io/xayn_b2b_demo/search_movies_2/
+
+.. toctree::
+   :maxdepth: 4
+   :includehidden:
+
+   getting_started/getting_started
+
+.. sidebar-links::
+   :caption: Api Reference
+
+   Front Office: <https://docs.xayn.com/front_office.html>
+   Back Office : <https://docs.xayn.com/back_office.html>
+
+.. sidebar-links::
+   :caption: Links
+   :github:
