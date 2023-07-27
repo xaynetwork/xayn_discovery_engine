ce48f9aded3ed8ca2141afe6dba8bf5be0a16799
Jan Petsche <58227040+janpetschexain@users.noreply.github.com>
2023-07-27 17:59:09 +0200

    rescale es knn score

--- web-api/openapi/CHANGELOG.md
+++ web-api/openapi/CHANGELOG.md
@@ -1,3 +1,7 @@
+# 2.5.0 - 2023-07-31
+
+- Generalize scores in search results
+
 # 2.4.1 - 2023-07-31
 
 - Clarify requirements for filters regarding the indexed document properties
--- web-api/src/personalization/routes.rs
+++ web-api/src/personalization/routes.rs
@@ -362,6 +362,7 @@ pub(crate) async fn personalize_documents_by(
 
     let tag_weights = storage::Tag::get(storage, user_id).await?;
 
+    normalize_knn_scores(&mut documents);
     rerank_by_scores(
         coi_system,
         &mut documents,
@@ -684,6 +685,9 @@ async fn semantic_search(
     .await?;
 
     if let Some(personalize) = personalize {
+        if matches!(strategy, SearchStrategy::Knn) {
+            normalize_knn_scores(&mut documents);
+        }
         personalize_knn_search_result(
             &storage,
             &state.config,
@@ -757,4 +761,11 @@ async fn personalize_knn_search_result(
     }
 
     Ok(())
+}
+
+/// Normalize knn similarity scores for `rerank_by_scores()`.
+fn normalize_knn_scores(documents: &mut [PersonalizedDocument]) {
+    for document in documents {
+        document.score = (document.score + 1.) / 2.;
+    }
 }
