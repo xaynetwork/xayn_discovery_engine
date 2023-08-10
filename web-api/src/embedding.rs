43a250a2aeaa806e675be794288f5742d8dc8bed
Robert Debug <robertt.debug@gmail.com>
2023-08-10 15:09:21 +0200

    sagemaker impl

--- web-api/src/embedding.rs
+++ web-api/src/embedding.rs
@@ -12,7 +12,10 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+use aws_config::retry::RetryConfig;
+use aws_sdk_sagemakerruntime::{config::Region, primitives::Blob, Client};
 use serde::{Deserialize, Deserializer, Serialize};
+use serde_json::json;
 use xayn_ai_bert::{AvgEmbedder, Config as EmbedderConfig, NormalizedEmbedding};
 
 use crate::{app::SetupError, error::common::InternalError, utils::RelativePathBuf};
@@ -21,6 +24,7 @@ use crate::{app::SetupError, error::common::InternalError, utils::RelativePathBu
 #[serde(tag = "type", rename_all = "snake_case", deny_unknown_fields)]
 pub enum Config {
     Pipeline(Pipeline),
+    Sagemaker(Sagemaker),
 }
 
 impl Default for Config {
@@ -54,24 +58,41 @@ impl Default for Pipeline {
     }
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(deny_unknown_fields)]
+pub struct Sagemaker {
+    pub(crate) endpoint: String,
+    pub(crate) target_model: Option<String>,
+    pub(crate) retry_max_attempts: Option<u32>,
+    pub(crate) aws_region: Option<String>,
+    pub(crate) aws_profile: Option<String>,
+}
+
 pub(crate) enum Embedder {
     Pipeline(AvgEmbedder),
+    Sagemaker {
+        client: Client,
+        endpoint: String,
+        target_model: Option<String>,
+    },
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct SagemakerResponse {
+    embeddings: Vec<NormalizedEmbedding>,
 }
 
 impl Embedder {
-    #[allow(clippy::unused_async)] // required for sagemaker
     pub(crate) async fn load(config: &Config) -> Result<Self, SetupError> {
         match config {
-            Config::Pipeline(Pipeline {
-                directory,
-                token_size,
-            }) => Self::load_pipeline(directory, *token_size),
+            Config::Pipeline(config) => Self::load_pipeline(config),
+            Config::Sagemaker(config) => Self::load_client(config).await,
         }
     }
 
-    fn load_pipeline(directory: &RelativePathBuf, token_size: usize) -> Result<Self, SetupError> {
-        let config = EmbedderConfig::new(directory.relative())?
-            .with_token_size(token_size)?
+    fn load_pipeline(config: &Pipeline) -> Result<Self, SetupError> {
+        let config = EmbedderConfig::new(RelativePathBuf::from(&config.directory).relative())?
+            .with_token_size(config.token_size)?
             .with_pooler();
         config.validate()?;
         let embedder = config.build()?;
@@ -79,7 +100,31 @@ impl Embedder {
         Ok(Self::Pipeline(embedder))
     }
 
-    #[allow(clippy::unused_async)] // required for sagemaker
+    async fn load_client(config: &Sagemaker) -> Result<Self, SetupError> {
+        let mut config_loader = aws_config::from_env();
+
+        if let Some(region) = &config.aws_region {
+            config_loader = config_loader.region(Region::new(region.clone()));
+        }
+        if let Some(profile) = &config.aws_profile {
+            config_loader = config_loader.profile_name(profile.clone());
+        }
+
+        config_loader = config_loader.retry_config(
+            RetryConfig::standard()
+                .with_max_attempts(1 + config.retry_max_attempts.unwrap_or_default()),
+        );
+
+        let sdk_config = config_loader.load().await;
+        let client = Client::new(&sdk_config);
+
+        Ok(Self::Sagemaker {
+            client,
+            endpoint: config.endpoint.clone(),
+            target_model: config.target_model.clone(),
+        })
+    }
+
     pub(crate) async fn run(&self, sequence: &str) -> Result<NormalizedEmbedding, InternalError> {
         match self {
             Embedder::Pipeline(embedder) => embedder
@@ -87,12 +132,60 @@ impl Embedder {
                 .map_err(InternalError::from_std)?
                 .normalize()
                 .map_err(InternalError::from_std),
+            Embedder::Sagemaker {
+                client,
+                endpoint,
+                target_model,
+            } => Self::run_sagemaker(client, endpoint, target_model, sequence).await,
         }
+    }
+
+    async fn run_sagemaker(
+        client: &Client,
+        endpoint: &str,
+        target_model: &Option<String>,
+        sequence: &str,
+    ) -> Result<NormalizedEmbedding, InternalError> {
+        let input = json!({
+            "inputs": [sequence],
+        });
+        let mut request = client
+            .invoke_endpoint()
+            .endpoint_name(endpoint)
+            .content_type("application/json")
+            .body(Blob::new(input.to_string()));
+
+        if let Some(target_model) = target_model {
+            request = request.target_model(target_model);
+        };
+
+        let res = request.send().await.map_err(|e| {
+            InternalError::from_message(format!("Failed to request sagemaker endpoint. Error: {e}"))
+        })?;
+
+        let body = res.body().ok_or(InternalError::from_message(
+            "Received sagemaker response without body.",
+        ))?;
+
+        serde_json::from_slice::<SagemakerResponse>(body.as_ref())
+            .map_err(|e| {
+                InternalError::from_message(format!(
+                    "Failed to deserialize sagemaker response body. Error: {e}"
+                ))
+            })?
+            .embeddings
+            .pop()
+            .ok_or(InternalError::from_message(
+                "Missing embedding in sagemaker response.",
+            ))
     }
 
     pub(crate) fn embedding_size(&self) -> usize {
         match self {
             Embedder::Pipeline(embedder) => embedder.embedding_size(),
+            Embedder::Sagemaker { .. } => {
+                unimplemented!("embedding_size is not implemented for the sagemaker embedder")
+            }
         }
     }
 }
@@ -106,7 +199,7 @@ mod tests {
     #[tokio::test]
     async fn test_embedder() {
         let config = Config::Pipeline(Pipeline {
-            directory: xaynia().unwrap().into(),
+            directory: xaynia().unwrap().display().to_string(),
             ..Pipeline::default()
         });
         let embedder = Embedder::load(&config).await.unwrap();
